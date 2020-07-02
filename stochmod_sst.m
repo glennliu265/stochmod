a7ad3adf13b3866b8d5081a3840cab32d7b33542
Glenn <glenn.y.liu@gmail.com>
2020-07-01 20:05:28 -0400

    Fixed bug in no entrain months
    -Fixed bug for skipping no entrainment months; it seems that for those months, an entrain term was still being calculated...
    - Added YO's correction method which seems to work now. Am getting the same results so the correlation calculation is not the issue

--- stochmod_sst.m
+++ stochmod_sst.m
@@ -10,6 +10,11 @@ T0     =  0;% Temperature Anomaly at time 0 [C]
 
 % Integration Options
 t_end   = 12*1000;% Timestep to integrate up to
+dt      = 60*60*24*30; % Timestepping (Will be used to multiply lambda
+usetau  = 1
+
+% Correlation Options
+detrendopt = 0;
 
 % Filtering Options (expressed in terms of timesteps
 % filter_cutoff = 240 % Filter out freqs below this # of timesteps
@@ -19,6 +24,7 @@ t_end   = 12*1000;% Timestep to integrate up to
 projpath = '/Users/gliu/Downloads/02_Research/01_Projects/01_AMV/02_stochmod/';
 scriptpath  = [projpath,'/03_Scripts/stochmod/'];
 datpath  = [projpath,'/01_Data/'];
+outpath  = [projpath,'/02_Figures/20200602/'];
 
 % Add paths
 addpath(scriptpath)
@@ -31,6 +37,7 @@ lonf = 330; % [0 360]
 latf = 50; % [-90 90]
 mon  = 3;
 
+
 % White Noise Generator Options
 rescaler  = 10^0; % Multiplier to rescale whitenoise
 genrand   = 1    ;   % Option to generate white noise (1 to gen, 0 to load)
@@ -40,9 +47,10 @@ savenoise = 1    ;   % Option to save generated white noise
 %% Script Start / Setup
 
 % Load damping variable (ensemble and lag averaged)
-damppath = '/Users/gliu/Downloads/02_Research/01_Projects/01_AMV/01_hfdamping/01_Data/'
-dampmat = 'ensavg_nhflxdamping_monwin3_sig020_dof082_mode4.mat'
-load([damppath,dampmat],'ensavg','LON1','LAT') 
+damppath = '/Users/gliu/Downloads/02_Research/01_Projects/01_AMV/01_hfdamping/01_Data/';
+dampmat = 'ensavg_nhflxdamping_monwin3_sig020_dof082_mode4.mat';
+load([damppath,dampmat],'ensavg','LON1','LAT') ;
+load([datpath,'stochrun.mat'])
 
 % Find the point
 [oid,aid] = findcoords(lonf,latf,2,{LON1,LAT});
@@ -66,13 +74,13 @@ else
 end
 
 
-monscale = 30*24*60*60;
 
+% 
+lambda_new = exp(-1* squeeze(ensavg(oid,aid,:))/(rho*cp0*h) * monscale);
+lambda_old = exp(-1* 1 ./ mean(TAUall,2));
 
-
-
-lambda = squeeze(ensavg(oid,aid,:))/(rho*cp0*h) * monscale;
-explam = exp(-lambda);
+% explam = exp(-lambda);
+% lambda = squeeze(ensavg(oid,aid,:))/(rho*cp0*h)
 
 
 % Preallocate
@@ -83,7 +91,7 @@ damp_ts = NaN(size(temp_ts));
 %% Run Model Forward
 for t = 1:t_end
 
-    m = mod(mon + t,12);
+    m = mod(t,12);
     if m == 0
         m = 12;
     end
@@ -91,7 +99,12 @@ for t = 1:t_end
     lambda = ensavg(oid,aid,m);
     
     % Use damping coefficient to compute e-folding time
-    l = lambda / (rho*cp0*h)*monscale;
+    if usetau == 1
+        l = 1 ./ nanmean(TAUall(m,:),2)
+    else
+        l = lambda / (rho*cp0*h);
+    end
+
     
     % Get mixed layer depth
     %h = mld(mld_lon,mld_lat,m);
@@ -109,10 +122,14 @@ for t = 1:t_end
     
     % Calculate temp at next timestep
     %T1 = T*-1*l + noise_term;
-    T1 = -1*l*T + noise_term;
+    if usetau == 1
+        T1 = exp(-l)*T + noise_term;
+    else
+        T1 = exp(-l*dt)*T + noise_term;
+    end
     
     % Record values
-    damp_ts(:,t) = l*T;
+    damp_ts(:,t) = exp(-1*l*dt)*T;
     noise_ts(:,t) = noise_term;
     temp_ts(:,t) = T1;
 end
@@ -120,13 +137,62 @@ end
 % Reshape to separate month and year dimensions
 tempr = reshape(temp_ts,12,length(temp_ts)/12);
 
+
+R_march2a = []
+R_march2a(1) = 1
+
+ for i=1:60
+          lag_yr=fix((i+kmonth-1)/12);
+          R_march2a(1+i)=yo_cor(temp_ts(kmonth:12:end-lag_yr*12)',temp_ts(kmonth+i:12:end)',-1,-1);
+          %R_march1a(1+i)=yo_cor(SST1(24+kmonth:12:end-lag_yr*12),SST1(24+kmonth+i:12:end),-1,-1);
+ end
+
 % Calculate autocorrelation
 tot_lag = 60; % Total Lag in Months
 kmonth  = 3 ; % Lag Base Month (Lag 0)
 lag_rng = 0:60;
-[corr_ts] = calc_lagcovar(tempr,tempr,lag_rng,kmonth)
+[corr_ts] = calc_lagcovar(tempr,tempr,lag_rng,kmonth,detrendopt)
+
+if detrendopt == 1
+    
+    R0 = NaN(60,40);
+    R1 = NaN(60,40);
+    R2 = NaN(60,40);
+       
+    for en = 1:40
+        
+        T0 = SSTall(:,en);
+        
+        T1 = SSTall(:,en);
+        T2 =
+        
+        
+        
+    end
+    
+    
+end
 
 
+%%
+figure(1)
+clf
+hold on
+
+lagplot = [0:60];
+plot(lagplot,AVG0,'LineWidth',2.5,'color','b')
+plot(lagplot,AVG1,'LineWidth',2.5,'color','c')
+plot(lagplot,AVG2,'LineWidth',2.5,'color','g')
+plot(lagplot,corr_ts,'LineWidth',2.5,'color','r')
+legend('CESM1','No-Entrain (YO)','Entrain (YO)','No-Entrain (GL)')
+
+title(sprintf('SST Autocorrelation for month %i',kmonth),'FontSize',20)
+grid on
+ylabel('Lags (Months)')
+xlabel('Correlation')
+
+figname = sprintf('%sacplot_mon%i_usetau%i_detrend%i.png',outpath,kmonth,usetau,detrendopt);
+saveas(gcf,figname,'png')
 
 
 
