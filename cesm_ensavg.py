deb40024266801c71fee4f6fbe61a6482b2fc12d
Glenn <glenn.y.liu@gmail.com>
2020-07-06 18:37:17 -0400

    cftime manual fix, cesm_ensavg
    - When xarray reads in CESM1 data, the months are shifted (begins in Feb instead of Jan). Added a conditional to manually check this and add a corrected time coordinate through preprocessing in the open_dataset step. Not sure why this error, occurs, but need to be careful when opening other datasets with this script.

--- cesm_ensavg.py
+++ cesm_ensavg.py
@@ -8,12 +8,9 @@ Test script to take the climatological monthly mean for a dataset and output it.
 @author: gliu
 """
 
-
-
-
 import xarray as xr
 import time
-
+import glob
 
 # # Note: change variable name in dataset
 # def preprocess(ds):
@@ -26,38 +23,69 @@ import time
 #     return ds
     
 
-
 # Set variable name
 varname = 'HMXL'
 
 # Variables to keep
-varlist = [varname,'TLON','TLAT','time']
+varlist = [varname,'TLONG','TLAT','time']
 
-# Set paths and glob expression
+#%% Set paths and glob expression
 ncpath = "/vortex/jetstream/climate/data1/yokwon/CESM1_LE/downloaded/ocn/proc/tseries/monthly/" + varname + "/"
 ncnames = "b.e11.B20TRC5CNBDRD.f09_g16.*.pop.h." + varname + ".*.nc"
 globby = ncpath+ncnames
 
-# Open dataset
+
+# Get list of variables for testing
+nclist = glob.glob(globby)
+nctest = nclist[0]
+
+# Open first file to see if cftime decoded correctly
+dstest = xr.open_dataset(nctest)
+if dstest.time.values[0].month != 1:
+    print("First Timestep is not January, but: %s" %  str(dstest.time.values[0]))
+    montherr = 1
+else:
+    montherr = 0
+
+#%% Open dataset
+
+
+# Preprocess variable if it is applicable
+def preprocess(ds):
+    if ds.time.values[0].month != 1:
+    
+        startyr = str(ds.time.values[0].year)
+        correctedtime = xr.cftime_range(start=startyr,end="2005-12-31",freq="MS",calendar="noleap") 
+        ds = ds.assign_coords(time=correctedtime) 
+    return ds
+        
+        
+# Open dataset with preprocessing
 ds = xr.open_mfdataset(globby,
                        concat_dim="ensemble",
  #                      combine="by_coords",
- #                      preprocess=preprocess,
+                       preprocess=preprocess,
                        parallel=True)
 
-
+#Correct time variable if applicable (might be not needed,can remove later)
+if montherr != 1:
+    startyr = str(ds.time.values[0].year)
+    correctedtime = xr.cftime_range(start=startyr,end="2005-12-31",freq="MS",calendar="noleap") 
+    ds['time'] = correctedtime
+    ds.assign_coords(time=correctedtime) 
+    print("Corrected Start to: %s; End to: %s" %  (str(ds.time.values[0]),str(ds.time.values[-1])))
+    
 # Drop variables not in list
 dsvars = list(ds.variables)
 remvar = [i for i in dsvars if i not in varlist]
-ds.drop(remvar)
-
+ds = ds.drop(remvar)
 
 # Slice to the proper time period
+# NOTE: one month offset in time (starts at 1920-02-01)
 ds = ds.sel(time=slice('1920-01-01','2005-12-31'))
 
 # Compute Monthly means
-varmon = ds[varname].groupby('time.month').mean('time')  
-
+varmon = ds[varname].groupby('time.month').mean('time')   
 
 # Save output
 start = time.time()
