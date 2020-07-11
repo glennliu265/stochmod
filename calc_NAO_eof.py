a38ae170162bc143c0f13a12286c84b8b9e94c22
Glenn <glenn.y.liu@gmail.com>
2020-07-10 20:52:00 -0400

    updated calc NAO Eof
    - Added new regression function and method into the loop
    - Added option to flip EOF based on sums within specified boxes
    -Added portion to load data back in for visualization
    - Functionized Regression plot
    - Added ensemble-all PC plot
    - Added loop to plot all ensemble members

--- calc_NAO_eof.py
+++ calc_NAO_eof.py
@@ -54,8 +54,63 @@ def eof_simple(pattern,N_mode,remove_timemean):
     return eofs, pcs, varexp
 
     
+def regress_2d(A,B):
+    """
+    Regresses A (independent variable) onto B (dependent variable), where
+    either A or B can be a timeseries [N-dimensions] or a space x time matrix 
+    [N x M]. Script automatically detects this and permutes to allow for matrix
+    multiplication.
+    
+    Returns the slope (beta) for each point, array of size [M]
     
     
+    """
+    # Determine if A or B is 2D and find anomalies
+    
+    
+    # 2D Matrix is in A [MxN]
+    if len(A.shape) > len(B.shape):
+        
+        # Tranpose A so that A = [MxN]
+        if A.shape[1] != B.shape[0]:
+            A = A.T
+        
+        
+        # Set axis for summing/averaging
+        a_axis = 1
+        b_axis = 0
+        
+        # Compute anomalies along appropriate axis
+        Aanom = A - np.nanmean(A,axis=a_axis)[:,None]
+        Banom = B - np.nanmean(B,axis=b_axis)
+        
+
+        
+    # 2D matrix is B [N x M]
+    elif len(A.shape) < len(B.shape):
+        
+        # Tranpose B so that it is [N x M]
+        if B.shape[0] != A.shape[0]:
+            B = B.T
+        
+        # Set axis for summing/averaging
+        a_axis = 0
+        b_axis = 0
+        
+        # Compute anomalies along appropriate axis        
+        Aanom = A - np.nanmean(A,axis=a_axis)
+        Banom = B - np.nanmean(B,axis=b_axis)[None,:]
+    
+    # Calculate denominator, summing over N
+    Aanom2 = np.power(Aanom,2)
+    denom = np.sum(Aanom2,axis=a_axis)    
+    
+    # Calculate Beta
+    beta = Aanom @ Banom / denom
+    
+        
+    return beta
+
     
 
 
@@ -83,7 +138,7 @@ lonpos  = slp.shape.index(lonsize)
 # EOF options
 N_mode = 3
 station = "Reykjavik"
-
+flipeof = 1
 
 lonLisbon = -9.1398 + 360
 latLisbon = 38.7223
@@ -91,6 +146,8 @@ lonReykjavik = -21.9426 + 360
 latReykjavik = 64.146621
 tol = 5 # in degrees
 
+# Regression method
+method = 3 # 1 = pt by pt loop with polyfit, #2 = just take raw eof, #3 = regress2d function (vectorized)
 
 
 # Find box indices
@@ -152,9 +209,6 @@ for e in range(slp.shape[0]):
     #pcall[e,:,:] = pcs / np.std(pcs,axis=0)
     pcraw = pcs / np.std(pcs,axis=0)
     
-    
-
-    
     #%% Reshape EOFs
     
     #varfill = np.zeros((data.shape[1],N_mode))
@@ -166,70 +220,96 @@ for e in range(slp.shape[0]):
     #     eofall[e,:,:,:] = np.reshape(eofrs,(lonsize,latsize,N_mode))
         
     #%% Regress Pcs back to variable...
-    eofrs = np.zeros((data.shape[1],N_mode))
-    for pc in range(pcs.shape[1]):
-        
-        # Get corresponding PC times series
-        ts = pcs[:,pc]
-        i = 0
-        
-        # Loop thru each pt
-        for pt in range(data.shape[1]):
+    
+    # Explicit Regression step
+    if method == 1:
+        eofrs = np.zeros((data.shape[1],N_mode))
+        for pc in range(pcs.shape[1]):
             
-            vartime = data[:,pt]
+            # Get corresponding PC times series
+            ts = pcs[:,pc]
+            i = 0
             
-            # Skip nan points
-            if np.any(np.isnan(vartime)):
-                eofrs[pt,pc]=np.nan
+            # Loop thru each pt
+            for pt in range(data.shape[1]):
                 
+                vartime = data[:,pt]
+                
+                # Skip nan points
+                if np.any(np.isnan(vartime)):
+                    eofrs[pt,pc]=np.nan
+                    
+                    i += 1
+                    continue
+                
+                # Perform regrssion
+                r = np.polyfit(ts,vartime,deg=1)
+                
+                eofrs[pt,pc] = r[0]
                 i += 1
-                continue
-            
-            # Perform regrssion
-            r = np.polyfit(ts,vartime,deg=1)
-            
-            eofrs[pt,pc] = r[0]
-            i += 1
-            msg = '\rCompleted Regression for %i of %i points for PC %i' % (i,data.shape[1],pc+1)
-            print(msg,end="\r",flush=True)
-        #print('\n')
-    
-    #%% Flip EOFs based on boxes...
-    
-    # EOF 1 only (need to figure out the other EOFs..)
-    # First check using reykjavik
-    rboxlon = np.where((lon >= lonReykjavik-tol) & (lon <= lonReykjavik+tol))[0]
-    rboxlat = np.where((lat >= latReykjavik-tol) & (lat <= latReykjavik+tol))[0]
-    
-    
-    eofraw = np.reshape(eofrs,(latsize,lonsize,N_mode))
-    chksum = np.sum(eofraw[rboxlat[:,None],rboxlon[None,:],0],(0,1))
-    if chksum > 0:
-        print("\t Flipping sign based on Reykjavik, Ens%i" % (e+1))
-        eofraw *= -1
-        pcraw *= -1
+                msg = '\rCompleted Regression for %i of %i points for PC %i' % (i,data.shape[1],pc+1)
+                print(msg,end="\r",flush=True)
+            #print('\n')
+    elif method == 2:
+                
+        eofrs = np.zeros((data.shape[1],N_mode))
+        eofrs[inotnan,:] = eofs
+    elif method == 3:
         
+        eofrs = np.zeros((data.shape[1],N_mode))
+        for pc in range(pcs.shape[1]):
+            eofrs[:,pc] = regress_2d(pcs[:,pc],data) 
+        
+        
+    #%% Flip EOFs based on boxes...
+    if flipeof == 1:
+        # EOF 1 only (need to figure out the other EOFs..)
+        # First check using reykjavik
+        rboxlon = np.where((lon >= lonReykjavik-tol) & (lon <= lonReykjavik+tol))[0]
+        rboxlat = np.where((lat >= latReykjavik-tol) & (lat <= latReykjavik+tol))[0]
+        
+        
+        eofraw = np.reshape(eofrs,(latsize,lonsize,N_mode))
+        chksum = np.sum(eofraw[rboxlat[:,None],rboxlon[None,:],0],(0,1))
+        if chksum > 0:
+            print("\t Flipping sign based on Reykjavik, Ens%i" % (e+1))
+            eofraw *= -1
+            pcraw *= -1
+            
+        
+    
+        # Double Check with Lisbon
+        lboxlon = np.where((lon >= lonLisbon-tol) & (lon <= lonLisbon+tol))[0]
+        lboxlat = np.where((lat >= latLisbon-tol) & (lat <= latLisbon+tol))[0]
+        
+        
+        eofraw = np.reshape(eofrs,(latsize,lonsize,N_mode))
+        chksum = np.sum(eofraw[lboxlat[:,None],lboxlon[None,:],0],(0,1))
+        if chksum < 0:
+            print("\t Flipping sign based on Lisbon, Ens%i" % (e+1))
+            eofraw *= -1
+            pcraw *= -1
+        #%% Assign to outside variable
+        eofall[e,:,:,:] = eofraw
+        pcall[e,:,:] = pcraw
+            
+        print("Calculated EOF for ens %i of %i in %.2fs\n" % (e+1,slp.shape[0],time.time()-startloop))
+    else:
+        eofall[e,:,:,:] = np.reshape(eofrs,(latsize,lonsize,N_mode))
+        pcall[e,:,:] = pcraw
     
 
-    # Double Check with Lisbon
-    lboxlon = np.where((lon >= lonLisbon-tol) & (lon <= lonLisbon+tol))[0]
-    lboxlat = np.where((lat >= latLisbon-tol) & (lat <= latLisbon+tol))[0]
-    
-    
-    eofraw = np.reshape(eofrs,(latsize,lonsize,N_mode))
-    chksum = np.sum(eofraw[lboxlat[:,None],lboxlon[None,:],0],(0,1))
-    if chksum < 0:
-        print("\t Flipping sign based on Lisbon, Ens%i" % (e+1))
-        eofraw *= -1
-        pcraw *= -1
-    #%% Assign to outside variable
-    eofall[e,:,:,:] = eofraw
-    pcall[e,:,:] = pcraw
-        
-    print("Calculated EOF for ens %i of %i in %.2fs\n" % (e+1,slp.shape[0],time.time()-startloop))
-    
-    
+#%% 
 np.savez(datpath+"Manual_EOF_Calc_NAO.npz",eofall=eofall,pcall=pcall,varexpall=varexpall)
+
+
+#%% Load Data
+npzdata = np.load(datpath+"Manual_EOF_Calc_NAO.npz")
+eofall    = npzdata['eofall']
+pcall     = npzdata['pcall']
+varexpall = npzdata['varexpall'] 
+
+
 
 # %% Try some plots
 
@@ -239,65 +319,113 @@ np.savez(datpath+"Manual_EOF_Calc_NAO.npz",eofall=eofall,pcall=pcall,varexpall=v
 N = 0
 e = 'avg'
 
-if e == "avg":
-    pexplained = np.nanmean(varexpall,0)[N]*100
-    var = np.nanmean(eofall[:,:,:,N],0)/100
-    estring = e
-    tsplot = np.nanmean(pcall,0)[:,N]
-else:
-    pexplained = varexpall[e-1,N]*100  
-    var = np.copy(eofall[e-1,:,:,N])/100 # Convert to milibars
-    estring = str(e)
-    tsplot = pcall[e,:,N]
-cmap = cmocean.cm.balance
-# NAO Calculation Settings
-lonW = -90
-lonE = 40
-latS = 20
-latN = 80
+def plot_nao_eof(N,e):
+    if e == "avg":
+        pexplained = np.nanmean(varexpall,0)[N]*100
+        var = np.nanmean(eofall[:,:,:,N],0)/100
+        estring = e
+        tsplot = np.nanmean(pcall,0)[:,N]
+    else:
+        pexplained = varexpall[e-1,N]*100  
+        var = np.copy(eofall[e-1,:,:,N])/100 # Convert to milibars
+        estring = str(e)
+        tsplot = pcall[e,:,N]
+    cmap = cmocean.cm.balance
+    # NAO Calculation Settings
+    lonW = -90
+    lonE = 40
+    latS = 20
+    latN = 80
+    
+    #cint = np.arange(-0.5,0.55,0.05)
+    if N == 0:
+        cint = np.arange(-3,3.5,0.5)
+    elif N == 1:
+        cint = np.arange(-1,1.2,0.2)
+    elif N == 2:
+        cint = np.arange(-2,2.1,0.1)
+    
+    
+    # Plot the EOF
+    #var1,lon1 = add_cyclic_point(var,lon) 
+    plt.style.use('ggplot')
+    fig,ax= plt.subplots(1,1,figsize=(6,4))
+    
+    ax = plt.axes(projection=ccrs.PlateCarree())
+    ax.set_extent([lonW,lonE,latS,latN])
+    
+    # Add filled coastline
+    ax.add_feature(cfeature.COASTLINE)
+    
+    # Add contours
+    cs = ax.contourf(lon,lat,var,cint,cmap=cmap,transform=ccrs.PlateCarree())
+    # Negative contours
+    cln = ax.contour(lon,lat,var,
+                cint[cint<0],
+                linestyles='dashed',
+                colors='k',
+                linewidths=0.5,
+                transform=ccrs.PlateCarree())
+    
+    # Positive Contours
+    clp = ax.contour(lon,lat,var,
+                cint[cint>=0],
+                colors='k',
+                linewidths=0.5,
+                transform=ccrs.PlateCarree())    
+    # Add Gridlines
+    gl = ax.gridlines(draw_labels=True,linewidth=0.75,color='black',linestyle=':')
+    
+    gl.xlabels_top = gl.ylabels_right = False
+    gl.xformatter = LONGITUDE_FORMATTER
+    gl.yformatter = LATITUDE_FORMATTER
+    bc = plt.colorbar(cs,orientation='horizontal')
+    
+    ax.set_title("EOF %i, Variance Explained: %.02f%% " %(N+1,pexplained),fontsize=16)
+    plt.savefig(outpath+"NAO_EOF%i_Ens%s.png"%(N+1,estring), bbox_inches="tight",dpi=200)
+    return fig
 
-#cint = np.arange(-0.5,0.55,0.05)
-cint = np.arange(-3,3.5,0.5)
+plot_nao_eof(0,"avg")
 
-# Plot the EOF
-#var1,lon1 = add_cyclic_point(var,lon) 
+
+for N in range(2):
+    for e in range(42):
+        plot_nao_eof(N,e)
+
+#%% Plot the PCs
+
 plt.style.use('ggplot')
-fig,ax= plt.subplots(1,1,figsize=(6,4))
+fig,ax = plt.subplots(3,1,figsize=(10,6),sharex=True)
 
-ax = plt.axes(projection=ccrs.PlateCarree())
-ax.set_extent([lonW,lonE,latS,latN])
+for N in range(3):
+    
+    plt.subplot(3,1,N+1)
+    
+    # Plot for each ensemble member
+    for e in range(42):
+        
+        varplot = np.squeeze(pcall[e,:,N])
+        plt.plot(year,varplot,color=[0.65,0.65,0.65])    
+    l1 = plt.plot(year,pcall[-1,:,N],color=[0.65,0.65,0.65],label="Indv. Member")
+    varensavg = np.nanmean(pcall[:,:,N],axis=0)
+    l2 = plt.plot(year,varensavg,color='k',label='Ensemble Average')
+    
+    
+    explained = np.nanmean(varexpall,0)[N]*100
+    plt.title("PC %i, Variance Explained: %.02f%% " %(N+1,explained),fontsize=16)
+    if N == 1:
+        plt.ylabel("NAO Index")
+    plt.ylim([-3,3])
+    
+plt.tight_layout()
+plt.xlabel("Years")    
+plt.savefig(outpath+"NAO_PCAll_EnsAll.png", bbox_inches="tight",dpi=200)
 
-# Add filled coastline
-ax.add_feature(cfeature.COASTLINE)
 
-# Add contours
-cs = ax.contourf(lon,lat,var,cint,cmap=cmap,transform=ccrs.PlateCarree())
-# Negative contours
-cln = ax.contour(lon,lat,var,
-            cint[cint<0],
-            linestyles='dashed',
-            colors='k',
-            linewidths=0.5,
-            transform=ccrs.PlateCarree())
 
-# Positive Contours
-clp = ax.contour(lon,lat,var,
-            cint[cint>=0],
-            colors='k',
-            linewidths=0.5,
-            transform=ccrs.PlateCarree())    
-# Add Gridlines
-gl = ax.gridlines(draw_labels=True,linewidth=0.75,color='black',linestyle=':')
 
-gl.xlabels_top = gl.ylabels_right = False
-gl.xformatter = LONGITUDE_FORMATTER
-gl.yformatter = LATITUDE_FORMATTER
-bc = plt.colorbar(cs,orientation='horizontal')
 
-ax.set_title("EOF %i, Variance Explained: %.02f%% " %(N+1,pexplained),fontsize=16)
-plt.savefig(outpath+"NAO_EOF%i_Ens%s.png"%(N+1,estring), bbox_inches="tight",dpi=200)
 
-# Plot the PC
 plt.style.use('ggplot')
 fig,ax= plt.subplots(1,1,figsize=(6,4))
 ax.plot(year,tsplot)
