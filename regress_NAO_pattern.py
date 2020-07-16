d2641ee84187cfb7d07e6e95f4f3b601b77fc10e
Glenn <glenn.y.liu@gmail.com>
2020-07-16 01:08:28 -0400

    regress NAO pattern updates
    - Generalized visualization to any flux, including THFLX and RHFLX
    - Added SLP overlay plots, though weird stuff is still going on with contour labeling (extra horizontal lines...)

--- regress_NAO_pattern.py
+++ regress_NAO_pattern.py
@@ -87,20 +87,17 @@ def regress_2d(A,B):
 # Path to data
 projpath = "/Users/gliu/Downloads/02_Research/01_Projects/01_AMV/02_stochmod/"
 datpath  = projpath + '01_Data/'
-outpath = projpath+'/02_Figures/20200714/'
+outpath = projpath+'/02_Figures/20200716/'
+flux = 'NHFLX' # [ 'FLNS','FSNS','LHFLX','SHFLX','RHFLX','THFLX','NHFLX','PSL']
 
-ncname = "NHFLX_NAOproc.nc"
+#ncname = "NHFLX_NAOproc.nc"
+
+#
+
 
 
 
 # %% Load data
-
-# Load NHFLX data
-ds = xr.open_dataset(datpath+ncname)
-nhflx = ds['NHFLX'].values
-lon = ds['lon'].values
-lat = ds['lat'].values
-time = ds['year'].values
 
 
 # Load Data (NAO Index)
@@ -109,10 +106,53 @@ eofall    = npzdata['eofall']
 pcall     = npzdata['pcall']
 varexpall = npzdata['varexpall'] 
 
+# Load Data base on flux (Combine separately for RHFLX and THFLX)
+if flux == 'THFLX':
+    
+    # Load Latent Heat Fluxes
+    ncname1 = 'LHFLX_NAOproc.nc'
+    ds1 = xr.open_dataset(datpath+ncname1)
+    lhflx = ds1['LHFLX']
+    
+    # Load Sensible Heat Fluxes
+    ncname2 = 'SHFLX_NAOproc.nc'
+    ds2 = xr.open_dataset(datpath+ncname2)
+    shflx = ds2['SHFLX']
+    
+    # Sum the two fluxes
+    flx = lhflx+shflx
+    
+elif flux == 'RHFLX':
+    
+    # Load Shortwave 
+    ncname1 = 'FSNS_NAOproc.nc'
+    ds1 = xr.open_dataset(datpath+ncname1)
+    fsns = ds1['FSNS']
+    
+    # Load Sensible Heat Fluxes
+    ncname2 = 'FLNS_NAOproc.nc'
+    ds2 = xr.open_dataset(datpath+ncname2)
+    flns = ds2['FLNS']
+    
+    # Sum the two fluxes
+    flx = fsns+flns
+    
+else:
+    
+    ncname = "%s_NAOproc.nc" % flux
+    ds = xr.open_dataset(datpath+ncname)
+    flx = ds[flux]
+    
+
+# Read in Coordinate values
+lon = flx['lon'].values
+lat = flx['lat'].values
+time = flx['year'].values
+flx = flx.values
 
 # %% Prepare data
 
-var = np.copy(nhflx)*-1 # Note, multiply by negative 1 to convert to upwards negative
+var = np.copy(flx)*-1 # Note, multiply by negative 1 to convert to upwards negative
 
 # Get dimension sizes
 nens,nyr,nlat,nlon = var.shape
@@ -139,32 +179,28 @@ varout = np.reshape(varpattern,(npc,nens,nlat,nlon))
 
 F = np.copy(varout)
 
-outname = datpath+"NAO_NHFLX_Forcing.npy"
+outname = datpath+"NAO_%s_Forcing.npy" % flux 
 np.save(outname,F)
 
 #%%
 # Try Plotting ensemble average
-
-
-
-
-def plot_regression(N,e,varin,lon,lat,cint=[0]):
-    
-    
+def plot_regression_slp(N,e,varin,slp,lon,lat,cint=[0]):
     # Restrict to Ensemble and PC
     if e == "avg":
         # Assumed [pc, ens, lat, lon]
         varplot = np.nanmean(varin,axis=1)[N,...]
+        slpplt = np.nanmean(slp,axis=0)[:,:,N]
         estring = "AVG"
     else:
         varplot = varin[N,e,:,:]
+        slpplt = slp[e,:,:,N]
         estring = "%02i"%e
     
     cmap = cmocean.cm.balance
     #Plotting boundaries
     lonW = -90
     lonE = 40
-    latS = 20
+    latS = 0
     latN = 80
 
     # if N == 0:
@@ -174,8 +210,9 @@ def plot_regression(N,e,varin,lon,lat,cint=[0]):
     # elif N == 2:
     #     cint = np.arange(-2,2.1,0.1)
     
-    
+    cintp = np.arange(-5,6,1)
     # Plot the EOF
+    slp1,lon1 = add_cyclic_point(slpplt,lon)
     var1,lon1 = add_cyclic_point(varplot,lon) 
     
     plt.style.use('ggplot')
@@ -192,23 +229,25 @@ def plot_regression(N,e,varin,lon,lat,cint=[0]):
         cs = ax.contourf(lon1,lat,var1,cmap=cmap,transform=ccrs.PlateCarree())
     else:
         cs = ax.contourf(lon1,lat,var1,cint,cmap=cmap,transform=ccrs.PlateCarree())
+        
         # Negative contours
-        cln = ax.contour(lon1,lat,var1,
-                    cint[cint<0],
+        cln = ax.contour(lon1,lat,slp1,
+                    cintp[cintp<0],
                     linestyles='dashed',
                     colors='k',
                     linewidths=1,
                     transform=ccrs.PlateCarree())
+        plt.clabel(cln,levels=[-2,-4],fmt='%i',fontsize=8)
         
         # Positive Contours
-        clp = ax.contour(lon1,lat,var1,
-                    cint[cint>=0],
+        clp = ax.contour(lon1,lat,slp1,
+                    cintp[cintp>=0],
                     colors='k',
                     linewidths=1,
                     transform=ccrs.PlateCarree())   
         # Add Label
-        plt.clabel(cln,fmt='%i',fontsize=8)
-        plt.clabel(clp,fmt='%i',fontsize=8)
+        
+        plt.clabel(clp,levels=[0,2,4],fmt='%i',fontsize=8)
     
     # Add Gridlines
     gl = ax.gridlines(draw_labels=True,linewidth=0.5,color='black',linestyle=':')
@@ -218,29 +257,32 @@ def plot_regression(N,e,varin,lon,lat,cint=[0]):
     
 
     bc = plt.colorbar(cs,orientation='horizontal')
-    ax.set_title("NHFLX - PC%i Regression, ENS %s, " %(N+1,estring),fontsize=16)
+    ax.set_title("%s - PC%i Regression, ENS %s, " %(flux,N+1,estring),fontsize=16)
 
 
     return fig
 N =0
 e = 'avg'
-cint = np.arange(-50,55,5)
+if flux == 'RHFLX':
+    cint = np.arange(-10,11,1)
+else:
+    cint = np.arange(-50,55,5)
 estring = str(e)
 
 
 
-fig = plot_regression(N,e,varout,lon,lat,cint=cint)
-plt.savefig(outpath+"NAO_EOF%i_Ens%s.png"%(N+1,estring), bbox_inches="tight",dpi=200)
+fig = plot_regression_slp(N,e,varout,eofall,lon,lat,cint=cint)
+plt.savefig(outpath+"%s_NAO_EOF%i_Ens%s.png"%(flux,N+1,estring), bbox_inches="tight",dpi=200)
 
 
 #
 #%%Loop for each ensemble member
-cint = np.arange(-50,55,5)
+#cint = np.arange(-50,55,5)
 
 for e in range(42):
-    estring = str(e)
-    fig = plot_regression(0,e,varout,lon,lat,cint=cint)
-    plt.savefig(outpath+"NAO_EOF%i_Ens%s.png"%(N+1,estring), bbox_inches="tight",dpi=200)
+    estring = str(e+1)
+    fig = plot_regression_slp(0,e,varout,eofall,lon,lat,cint=cint)
+    plt.savefig(outpath+"%s_NAO_EOF%i_Ens%s.png"%(flux,N+1,estring), bbox_inches="tight",dpi=200)
 
 #%%
 
@@ -313,6 +355,94 @@ gl.xformatter = LONGITUDE_FORMATTER
 gl.yformatter = LATITUDE_FORMATTER
 bc = plt.colorbar(cs,orientation='horizontal')
 ax.set_title("NHFLX - PC%i Regression, ENS %s, " %(N+1,estring),fontsize=16)
+
+
+
+#%% Plots just considering heat flux...
+
+# Try Plotting ensemble average
+def plot_regression(N,e,varin,lon,lat,cint=[0]):
+    # Restrict to Ensemble and PC
+    if e == "avg":
+        # Assumed [pc, ens, lat, lon]
+        varplot = np.nanmean(varin,axis=1)[N,...]
+        estring = "AVG"
+    else:
+        varplot = varin[N,e,:,:]
+        estring = "%02i"% (e+1)
+    
+    cmap = cmocean.cm.balance
+    #Plotting boundaries
+    lonW = -90
+    lonE = 40
+    latS = 0
+    latN = 80
+
+    # if N == 0:
+    #     cint = np.arange(-3,3.5,0.5)
+    # elif N == 1:
+    #     cint = np.arange(-1,1.2,0.2)
+    # elif N == 2:
+    #     cint = np.arange(-2,2.1,0.1)
+    
+    
+    # Plot the EOF
+    var1,lon1 = add_cyclic_point(varplot,lon) 
+    
+    plt.style.use('ggplot')
+    fig,ax= plt.subplots(1,1,figsize=(6,4))
+    
+    ax = plt.axes(projection=ccrs.PlateCarree())
+    ax.set_extent([lonW,lonE,latS,latN])
+    
+    # Add filled coastline
+    ax.add_feature(cfeature.COASTLINE,linewidth=0.75,linestyle=":")
+    
+    # Add contours
+    if len(cint) == 1:
+        cs = ax.contourf(lon1,lat,var1,cmap=cmap,transform=ccrs.PlateCarree())
+    else:
+        cs = ax.contourf(lon1,lat,var1,cint,cmap=cmap,transform=ccrs.PlateCarree())
+        # Negative contours
+        cln = ax.contour(lon1,lat,var1,
+                    cint[cint<0],
+                    linestyles='dashed',
+                    colors='k',
+                    linewidths=1,
+                    transform=ccrs.PlateCarree())
+        
+        # Positive Contours
+        clp = ax.contour(lon1,lat,var1,
+                    cint[cint>=0],
+                    colors='k',
+                    linewidths=1,
+                    transform=ccrs.PlateCarree())   
+        # Add Label
+        plt.clabel(cln,fmt='%i',fontsize=8)
+        plt.clabel(clp,fmt='%i',fontsize=8)
+    
+    # Add Gridlines
+    gl = ax.gridlines(draw_labels=True,linewidth=0.5,color='black',linestyle=':')
+    gl.xlabels_top = gl.ylabels_right = False
+    gl.xformatter = LONGITUDE_FORMATTER
+    gl.yformatter = LATITUDE_FORMATTER
+    
+
+    bc = plt.colorbar(cs,orientation='horizontal')
+    ax.set_title("%s - PC%i Regression, ENS %s, " %(flux,N+1,estring),fontsize=16)
+
+
+    return fig
+N =0
+e = 'avg'
+cint = np.arange(-50,55,5)
+estring = str(e)
+
+
+
+fig = plot_regression(N,e,varout,lon,lat,cint=cint)
+plt.savefig(outpath+"%s_NAO_EOF%i_Ens%s.png"%(flux,N+1,estring), bbox_inches="tight",dpi=200)
+
 
 #%% Ensemble and annual average of nhflux
 
@@ -391,7 +521,7 @@ ax.set_title("NHFLX - PC%i Regression, ENS %s, " %(N+1,estring),fontsize=16)
     #Plotting boundaries
     lonW = -90
     lonE = 40
-    latS = 20
+    latS = 0
     latN = 80
 
     # if N == 0:
