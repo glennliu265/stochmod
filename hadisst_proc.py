ac4c9919f522d1a87b667c8c271f956ed6d79044
Glenn <glenn.y.liu@gmail.com>
2020-07-23 19:28:17 -0400

    Updates to hadisst_proc
    - Updated function documentation
    - Removed Normalization in AMO Calculations
    - Added init_map variable from stochmod_explore
    - Option to remove seasonal cycle first then plot
    - More exploratory plots, including entrain model output

--- hadisst_proc.py
+++ hadisst_proc.py
@@ -94,7 +94,6 @@ def detrendlin(var_in):
     return var_detrend
 
 
-
 def area_avg(data,bbox,lon,lat,wgt):
     """
     Function to find the area average of data [lon x lat x otherdim]
@@ -151,30 +150,34 @@ def area_avg(data,bbox,lon,lat,wgt):
     return data_aa
 
 
-
-
 def calc_AMV(lon,lat,sst,bbox,order,cutofftime,awgt):
     """
+    Calculate AMV Index for detrended/anomalized SST data [LON x LAT x Time]
+    given bounding box [bbox]. Applies cosine area weighing
     
 
     Parameters
     ----------
-    lon : TYPE
-        DESCRIPTION.
-    lat : TYPE
-        DESCRIPTION.
-    sst : TYPE
-        DESCRIPTION.
-    bbox : TYPE
-        DESCRIPTION.
-    order : TYPE
-        DESCRIPTION.
-    cutofftime : TYPE
-        DESCRIPTION.
-
+    lon : ARRAY [LON]
+        Longitude values
+    lat : ARRAY [LAT]
+        Latitude Values
+    sst : ARRAY [LON x LAT x TIME]
+        Sea Surface Temperature
+    bbox : ARRAY [LonW,LonE,LonS,LonN]
+        Bounding Box for Area Average
+    order : INT
+        Butterworth Filter Order
+    cutofftime : INT
+        Filter Cutoff, expressed in same timesteps as input data
+        
     Returns
     -------
-    amv
+    amv: ARRAY [TIME]
+        AMV Index (Not Standardized)
+    
+    aa_sst: ARRAY [TIME]
+        Area Averaged SST
 
     """
     
@@ -187,21 +190,10 @@ def calc_AMV(lon,lat,sst,bbox,order,cutofftime,awgt):
     from scipy.signal import butter,filtfilt
     """
     
-    
     # Take the weighted area average
     aa_sst = area_avg(sst,bbox,lon,lat,awgt)
-    
-    # Linearly detrend the data
-    aa_sst = detrendlin(aa_sst)
-    
-    # Normalize the data
-    #sstmean = np.nanmean(aa_sst)
-    #sststd  = np.nanstd(aa_sst)
-    #sstanom = (aa_sst - sstmean) / sststd
-    
-    sstanom = aa_sst
-    
-    
+
+
     # Design Butterworth Lowpass Filter
     filtfreq = len(aa_sst)/cutofftime
     nyquist  = len(aa_sst)/2
@@ -209,7 +201,7 @@ def calc_AMV(lon,lat,sst,bbox,order,cutofftime,awgt):
     b,a    = butter(order,cutoff,btype="lowpass")
     
     # Compute AMV Index
-    amv = filtfilt(b,a,sstanom)
+    amv = filtfilt(b,a,aa_sst)
     
     return amv,aa_sst
     
@@ -619,11 +611,36 @@ def detrend_dim(invar,dim):
     
     return dtvar,linmod,beta,intercept
     
-   
-#%%
-
-
+def init_map(bbox,ax=None):
+    """
+    Quickly initialize a map for plotting
+    """
+    # Create Figure/axes
+    #fig = plt.gcf() 
+    
+    #ax = fig.add_subplot(1,1,1,projection=ccrs.PlateCarree())
+    if ax is None:
+        ax = plt.gca()
+    #ax = plt.axes(projection=ccrs.PlateCarree())
+        
+    
+    ax.set_extent(bbox)
+    
+    # Add Filled Coastline
+    ax.add_feature(cfeature.COASTLINE,facecolor='k')
+    
+    # Add Gridlines
+    gl = ax.gridlines(draw_labels=True,linewidth=0.5,color='gray',linestyle=':')
+    gl.xlabels_top = gl.ylabels_right = False
+    gl.xformatter = LONGITUDE_FORMATTER
+    gl.yformatter = LATITUDE_FORMATTER
+    
+    return ax
+#%% Load in HadISST Data
 # Path to SST data from obsv
+projpath = "/Users/gliu/Downloads/02_Research/01_Projects/01_AMV/02_stochmod/"
+outpath = projpath + '02_Figures/20200723/'
+datpath = projpath + '01_Data/'
 datpath2 = "/Users/gliu/Downloads/02_Research/01_Projects/01_AMV/00_Commons/01_Data/"
 
 # Load in observation SST data to compare
@@ -644,7 +661,7 @@ monstart = (1920+1-hyr[0,0])*12
 hsst = hsst[:,:,monstart::]
 
 
-#%% For hsst, flip the latitude axis
+#%% Fix Latitude Dimensions for HSST
 # currently it is arranged 90:-90, need to flip to south first
 
 # Find north and south latitude points
@@ -663,7 +680,8 @@ hsstnew = np.concatenate((hsstsouth,hsstnorth),axis=1)
 
 
 
-#%% Perform Linear Detrend on SST and annually averaged sst
+#%% Perform Linear Detrend on SST and annually averaged sst (Detrend First)
+
 usedtfunction = 1  # Set to 1 to use the new detrending function
 
 if usedtfunction == 1:
@@ -703,9 +721,9 @@ else:
 
 
 
-#%% Visualize detrending for point [lonf,latf]
+#%% Visualize detrending for point [lonf,latf] (Visualize Detrending effect)
 
-
+# Find Point
 lonf = -30
 latf = 64
 klon,klat = find_latlon(lonf,latf,hlon,hlatnew)
@@ -732,8 +750,7 @@ ax.scatter(tper,tempts,color='k',label="raw")
 ax.plot(tper,ymodts,color='b',label="linear model")
 plt.legend()
 
-#%% Remove Seasonal Cycle
-
+#%% Remove Seasonal Cycle (After Detrending)
 
 # Deseasonalize [lon x lat x yr x mon]
 ahsst = np.reshape(dt_hsst,(360,180,int(dt_hsst.shape[2]/12),12))
@@ -741,7 +758,8 @@ ahsst = ahsst - np.mean(ahsst,axis=2)[:,:,None,:]
 ahsst = np.reshape(ahsst,(360,180,dt_hsst.shape[2]))
 
 
-#%% Plot climatological removal
+#%% Visualize Deseasonalization...
+
 ats = ahsst[klon,klat,:]
 
 fig,ax = plt.subplots(1,1,figsize=(8,4))
@@ -752,7 +770,7 @@ plt.plot(tper,ats,color='r',label="Detrended,Deseasonalized")
 plt.legend()
 
 
-#%% Save Data
+#%% Save Data (Detrended First, then deseasonalized)
 
 timecft = xr.cftime_range(start="1920-01-01",end="2017-12-01",freq="MS") 
 
@@ -763,26 +781,50 @@ da = xr.DataArray(ahsst,
 da.name = 'SST'
 da.to_netcdf("%sHadISST_Detrended_Deanomalized_1920_2018.nc" % (datpath2))
 
-aa = xr.open_dataset("%sHadISST_Detrended_Deanomalized_1920_2018.nc" % (datpath2))
+#aa = xr.open_dataset("%sHadISST_Detrended_Deanomalized_1920_2018.nc" % (datpath2))
 
 
-#%% Sample AMV Calculations....
+#%% Try removing seasonal cycle first, then detrending
+
+# Remove Seasonal Cycle first and plot
+dsfirst = np.reshape(hsstnew,(360,180,int(dt_hsst.shape[2]/12),12))
+dsfirst = dsfirst - np.mean(dsfirst,axis=2)[:,:,None,:]
+dsfirst = np.reshape(dsfirst,(360,180,hsstnew.shape[2]))
+
+# Detrend
+start= time.time()
+dtdsfirst,dsymodall,_,_ = detrend_dim(dsfirst,2)
+print("Detrended in %.2fs" % (time.time()-start))
+
+# Plot Seasonal Cycle Removal and Detrended
+lonf = -30
+latf = 64
+tper = np.arange(0,hsstnew.shape[2])
+klon,klat = find_latlon(lonf,latf,hlon,hlatnew)
+fig,ax = plt.subplots(1,1,figsize=(8,4))
+ax.plot(tper,hsstnew[klon,klat,:],color='k',label="raw")
+ax.plot(tper,dsfirst[klon,klat,:],color='b',label="deseasonalized")
+ax.plot(tper,dtdsfirst[klon,klat,:],color='r',label="deseasonalized,detrended")
+ax.set_title("Deseasonalize First")
+plt.legend()
+
+#%% Sample AMV Calculations.... <Detrended First, then Deseasonalized>
 
 bbox  = [-80,0,0,60]
 order = 5
 cutofftime = 120 
 cmap = cmocean.cm.balance
-cint = np.arange(-1,1.1,0.1)
+cint = np.arange(-0.8,0.9,0.1)
 clab = cint
 # Detrended, Deseasonalized
 dtamv,dtaa = calc_AMV(hlon,hlatnew,ahsst,bbox,order,cutofftime,1)
 
-dtr = regress2ts(ahsst,dtamv,0,1)
+dtr = regress2ts(ahsst,dtamv/np.std(dtamv),0,1)
 
 plot_AMV_spatial(dtr.T,hlon,hlatnew,bbox,cmap,cint=cint,clab=clab)
 plt.title("Detrended, Deanomalized, Monthly SST")
 
-#%%  Detrended only
+#%%  AMV, <Detrended Only>
 dtamv,dtaa = calc_AMV(hlon,hlatnew,dt_hsst,bbox,order,cutofftime,1)
 
 dtr = regress2ts(dt_hsst,dtamv,0,1)
@@ -790,7 +832,7 @@ dtr = regress2ts(dt_hsst,dtamv,0,1)
 plot_AMV_spatial(dtr.T,hlon,hlatnew,bbox,cmap)
 plt.title("Detrended Monthly SST")
 
-#%% Raw
+#%% AMV <Raw>
 
 dtamv,dtaa = calc_AMV(hlon,hlatnew,hsstnew,bbox,order,cutofftime,1)
 
@@ -799,29 +841,19 @@ dtr = regress2ts(hsstnew,dtamv,0,1)
 plot_AMV_spatial(dtr.T,hlon,hlatnew,bbox,cmap)
 plt.title("Raw Monthly SST")
 
-
-
 #%% Trying annually averaged data
-
-
 
 aaraw = ann_avg(hsstnew,2)
 #dtraw = ann_avg(dt_hsst,2)
 #adtraw = ann_avg(ahsst,2)
 
-
-
 # Detrend annual data
-
-
-invar = aaraw
-cutofftime = 10
+invar = ahsst
+cutofftime = 120
 dtamv,dtaa = calc_AMV(hlon,hlatnew,invar,bbox,order,cutofftime,1)
 dtr = regress2ts(invar,dtamv,0,1)
 xtk = np.arange(1,101,10)
 xlbs = np.arange(1920,2020,10)
-
-
 
 #plt.subplots(211)
 plot_AMV_spatial(dtr.T,hlon,hlatnew,bbox,cmap,cint=cint,clab=clab)
@@ -831,9 +863,12 @@ plot_AMV_spatial(dtr.T,hlon,hlatnew,bbox,cmap,cint=cint,clab=clab)
 plot_AMV(dtamv)
 plt.xticks(xtk,xlbs)
 
-#%% Perform detrend on annually averaged sst (raw)  and try calculation from there
+#%% Perform detrend on annually averaged sst and try calculation from there
 
-annsst = ann_avg(hsstnew,2)
+
+
+selvar = dtdsfirst
+annsst = ann_avg(selvar,2)
 
 start= time.time()
 dtann,ymodann,_,_ = detrend_dim(annsst,2)
@@ -848,11 +883,152 @@ xtk = np.arange(1,101,10)
 xlbs = np.arange(1920,2020,10)
 
 
-cint = np.arange(-0.5,0.6,0.1)
+fig,ax = plt.subplots(1,1,figsize=(6,4))
+cint = np.arange(-0.8,0.9,0.1)
 #plt.subplots(211)
-plot_AMV_spatial(dtr.T,hlon,hlatnew,bbox,cmap,cint=cint,clab=cint)
-plot_AMV_spatial(dtr.T,hlon,hlatnew,bbox,cmap)
-#plt.title("Detrended, Deanomalized, Monthly SST")
+plot_AMV_spatial(dtr.T,hlon,hlatnew,[-80,0,0,80],cmap,cint=cint,clab=cint)
+#plot_AMV_spatial(dtr.T,hlon,hlatnew,bbox,cmap)
+plt.title("AMV Spatial Pattern ($^{\circ}C/\sigma_{AMV Indx})$ \n HadISST (1920-2018)")
+plt.savefig(outpath+"HADISST_AMV_Annual_LinDetrend_Deseas_First.png",dpi=200)
 
+fig,ax = plt.subplots(1,1,figsize=(6,2))
 plot_AMV(dtamv)
 plt.xticks(xtk,xlbs)
+plt.title("AMV Index HadISST (1920-2018)")
+plt.savefig(outpath+"HADISST_AMVIndex_Annual_LinDetrend.png",dpi=200)
+
+#%% Explore Differences between if you Detrend first or Deseasonalize first...
+
+diff = ahsst - dtdsfirst
+diffmax = np.nanmax(diff,axis=2)
+
+fig.ax = plt.subplots(1,1,figsize=(12,8))
+cint = np.arange(0,0.11,0.01)
+#pcm = plt.contourf(hlon,hlatnew,np.squeeze(diffmax).T,cmap=cmocean.cm.dense,vmax=0.10,vmin=0)
+pcm = plt.contourf(hlon,hlatnew,np.squeeze(diffmax).T,cint,cmap=cmocean.cm.dense)
+plt.colorbar(pcm)
+plt.title("Detrend first - Deseasonalized First (Max Differencem HadISST)")
+plt.savefig(outpath+"HADISST_Differences_dtfirst.png",dpi=200)
+
+#%% Explore Differences between if you use monthly versus annual data
+
+annsst = ann_avg(dtdsfirst,2)
+annamv,annaa = calc_AMV(hlon,hlatnew,annsst,bbox,order,cutofftime,1)
+annregr = regress2ts(annsst,annamv/np.nanstd(annamv),0,1)
+
+monamv,monaa = calc_AMV(hlon,hlatnew,dtdsfirst,bbox,order,cutofftime,1)
+monregr = regress2ts(dtdsfirst,monamv/np.nanstd(monamv),0,1)
+
+diff = annregr - monregr
+
+fig,axs = plt.subplots(1,1,figsize=(12,8),subplot_kw={'projection': ccrs.PlateCarree()})
+axs = init_map(bbox,ax=axs)
+cint=np.arange(-0.16,0.20,0.04)
+pcm=axs.contourf(hlon,hlatnew,diff.T,cint,cmap=cmap)
+axs.set_title("Annual - Monthly, Max Difference (AMV Pattern)")
+plt.colorbar(pcm)
+plt.savefig(outpath+"HADISST_AMV_Ann_vs_Mon_dsfirst.png",dpi=200)
+
+
+
+#%% Plot Maximum Anomaly
+
+
+maxanom = np.max(np.abs(dtdsfirst),axis=2)
+
+fig,axs = plt.subplots(1,1,figsize=(12,8),subplot_kw={'projection': ccrs.PlateCarree()})
+axs = init_map(bbox,ax=axs)
+cint=np.arange(0,11,1)
+pcm=axs.contourf(hlon,hlatnew,maxanom.T,cint,cmap=cmocean.cm.dense)
+axs.set_title("HadISST (1920-2018), Max SST Anomaly",fontsize=20)
+
+plt.colorbar(pcm)
+plt.savefig(outpath+"HADISST_AMV_MaxAnom_dsfirst.png",dpi=200)
+
+#%% Plot time series at a single point
+
+lonf = -50
+latf = 12
+
+klon,klat = find_latlon(lonf,latf,hlon,hlatnew)
+
+#sstpt = dtdsfirst[klon,klat,:]
+sstpt = dtdsfirst[klon,klat,:]
+
+fig,ax = plt.subplots(1,1,figsize=(6,3))
+plt.style.use('ggplot')
+plt.plot(tper,sstpt)
+plt.ylabel("SST Anomaly ($^{\circ}C$)")
+plt.xlabel("Time(Months)")
+plt.title("Detrended, Deseasonalized SST at LON: %02d LAT: %02d \n Mean: %.2f || Std: %.2f || Max: %.2f" % (lonf,latf,np.nanmean(sstpt),np.nanstd(sstpt),np.nanmax(np.abs(sstpt))))
+plt.tight_layout()
+
+plt.savefig(outpath+"HadISST_dsdt_SST_lon%02d_lat%02d.png"%(lonf,latf),dpi=200)
+
+
+
+#%% Try with entrain data
+# Load data and plot
+funiform = 2
+entraint = np.load(datpath+"stoch_output_1000yr_funiform2_entrain1_hvar2.npy")
+Fn =np.load(datpath+"stoch_output_1000yr_run000_funiform2_Forcing.npy",allow_pickle=True)
+#forcing = np.load(datpath+"stoch_output_1000yr_funiform%i_Forcing.npy"%(funiform))
+lonr = np.load(datpath+"lon.npy")
+latr = np.load(datpath+"lat.npy")
+
+maxanom = np.max(np.abs(entraint),axis=2)
+
+fig,axs = plt.subplots(1,1,figsize=(12,8),subplot_kw={'projection': ccrs.PlateCarree()})
+axs = init_map(bbox,ax=axs)
+#cint=np.arange(0,11,1)
+pcm=axs.contourf(lonr,latr,maxanom.T,cmap=cmocean.cm.dense)
+axs.set_title("Entrain, Max SST Anomaly",fontsize=20)
+
+plt.colorbar(pcm)
+plt.savefig(outpath+"Entrain_MaxAnom_dsfirst.png",dpi=200)
+
+#%% Plot Point for entrain data...
+
+
+lonf = -30
+latf = 50
+
+klon,klat = find_latlon(lonf,latf,lonr,latr)
+
+
+sstpt = entraint[klon,klat,:]
+F=Fn.item().get(0)[klon,klat,:]
+
+
+tper = np.arange(0,entraint.shape[2])
+fig,ax = plt.subplots(2,1,figsize=(6,4))
+plt.style.use('ggplot')
+
+
+plt.subplot(2,1,1)
+plt.plot(tper,F)
+plt.ylabel("Forcing ($^{\circ}C/s$)",fontsize=10)
+plt.title("Forcing at LON: %02d LAT: %02d \n Mean: %.2f || Std: %.2f || Max: %.2f" % (lonf,latf,np.nanmean(F),np.nanstd(F),np.nanmax(np.abs(F))))
+
+
+
+plt.subplot(2,1,2)
+plt.plot(tper,sstpt)
+plt.ylabel("SST ($^{\circ}C$)",fontsize=10)
+plt.xlabel("Time(Months)",fontsize=10)
+#plt.title("Detrended, Deseasonalized SST at LON: %02d LAT: %02d \n Mean: %.2f || Std: %.2f || Max: %.2f" % (lonf,latf,np.nanmean(sstpt),np.nanstd(sstpt),np.nanmax(np.abs(sstpt))))
+plt.title("Detrended, Deseasonalized SST \n Mean: %.2f || Std: %.2f || Max: %.2f" % (np.nanmean(sstpt),np.nanstd(sstpt),np.nanmax(np.abs(sstpt))))
+
+
+plt.tight_layout()
+
+plt.savefig(outpath+"HadISST_dsdt_SST_lon%02d_lat%02d_hvarmode%i_fscale%i.png"%(lonf,latf,hvarmode,fscale),dpi=200)
+
+
+
+
+
+
+
+
+
