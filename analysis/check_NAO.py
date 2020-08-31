31b15053395fa7ae52b9ea898610d40d91ca4e5b
Glenn <glenn.y.liu@gmail.com>
2020-08-31 18:56:39 -0400

    Added presentation plots
    Added some plots in similar style to damping and MLD plots for the presentation

--- analysis/check_NAO.py
+++ analysis/check_NAO.py
@@ -34,13 +34,15 @@ from cartopy.util import add_cyclic_point
 projpath = "/Users/gliu/Downloads/02_Research/01_Projects/01_AMV/02_stochmod/"
 scriptpath = projpath + '03_Scripts/stochmod/'
 datpath = projpath + '01_Data/'
-outpath = projpath + '02_Figures/20200820/'
-
+outpath = projpath + '02_Figures/20200823/'
+naotype = 0
 # File Name
-datfile = "NAO_Monthly_Regression_PC123.npz"
+datfile = "NAO_Monthly_Regression_PC123_naotype%i.npz" % naotype
 
 # Mapping box
 bbox = [260,20,0,90]
+# # Mapping box for forcing
+# [-100,40,-20,90]
 
 # Load latlon
 ll = loadmat("/Users/gliu/Downloads/02_Research/01_Projects/01_AMV/00_Commons/01_Data/CESM1_LATLON.mat")
@@ -145,6 +147,7 @@ def plot_regression(varin,lon,lat,cint=[0],ax=None,bbox=[-90,40,0,80],latlonlab=
 
     return ax
 
+
 #%% Create plots for all 42 ensemble members, separately for each month
 
 varin = np.copy(eof)
@@ -193,8 +196,11 @@ for m in range(12):
 eapbox = [-30,15,35,60]
 ebx    = [320,15,35,60]
 cint = np.arange(-2.4,2.6,0.2)
-# Take annual mean (OR DJFM, as currently indicated)
-varin = np.nanmean(np.copy(eof[:,[11,0,1],:,:,:]),1) # [ens x lat x lon x pc]
+if naotype > 1:
+    # Take annual mean (OR DJFM, as currently indicated)
+    varin = np.nanmean(np.copy(eof[:,[11,0,1],:,:,:]),1) # [ens x lat x lon x pc]
+else:
+    varin = np.copy(eof)
 patname = "SLP"
 meanname = 'DJFM'
 
@@ -213,9 +219,8 @@ for N in range(3):
         # Restrict to Ensemble,PC,month
         varplot = varin[e,:,:,N]
         
-        varexp1  = np.nanmean(varexp[e,:,N])
+        varexp1  = np.nanmean(varexp[e,...,N])
        
-        
         # Decide on Row or Column
         if e%6 == 0:
             colid = 0
@@ -239,7 +244,7 @@ for N in range(3):
         ax.set_title("Ens%02d %.2f"%(e+1,varexp1*100) + r"%")
         #ax.set_title("Ens%02d %i"%(e+1,chk))
     plt.tight_layout()
-    plt.savefig("%s%sPattern_PC%i_%sAvg.png" % (outpath,patname,N+1,meanname),dpi=200)
+    plt.savefig("%s%sPattern_PC%i_%sAvg_naotype%i.png" % (outpath,patname,N+1,meanname,naotype),dpi=200)
     print("Finished PC %i" % (N+1))
         
         
@@ -250,6 +255,60 @@ fig,ax = plt.subplots(1,1,figsize=(12,12))
 cf = ax.contourf(lon,lat,varplot,cint,cmap=cmocean.cm.balance)
 plt.colorbar(cf,ax=ax)
 plt.savefig(outpath+"colorbarscrap.png",dpi=200)
+
+#%% Make same plots (DJFM, all ens)m but for the older data [made by calc_NAO_eof.py]
+
+load2 = np.load(datpath+"Manual_EOF_Calc_NAO.npz")
+oleof = load2['eofall'] # ens x lat x lon x pc
+olexp = load2['varexpall'] # ens x pc
+patname="SLP"
+
+meanname="DJFM-OLD"
+eapbox = [-30,15,35,60]
+ebx    = [320,15,35,60]
+cint = np.arange(-2.4,2.6,0.2)
+for N in range(3):
+    
+    # Initialize figure
+    fig,axs = plt.subplots(7,6,subplot_kw={'projection': ccrs.PlateCarree()},figsize=(12,12))
+    
+    rowid = 0
+    colid = 0
+    
+    for e in range(42):
+        
+        # Restrict to Ensemble,PC,month
+        varplot = oleof[e,...,N]
+        
+        varexp1  = olexp[e,N]
+       
+        # Decide on Row or Column
+        if e%6 == 0:
+            colid = 0
+        else:
+            colid += 1
+        rowid = int(np.fix(e/6))
+        
+        # Create Plot
+        ax = axs[rowid,colid]
+        ax = plot_regression(varplot,lon,lat,ax=ax,cint=cint,cbar=False)
+        ax = viz.plot_box(eapbox,ax=ax)
+        
+        # Sum values to check for sign (transpose to lon x lat for format)
+        #rsum = proc.sel_region(varplot.T,lon,lat,ebx,reg_sum=1)
+        
+        #asum,lor,lar = proc.sel_region(varplot.T,lon,lat,ebx,reg_sum=0)
+        #chk  = rsum > 0 # set to 1 if it needs to be flipped
+        
+        #print("row %i col %i e%i"%(rowid,colid,e+1)) d
+
+        ax.set_title("Ens%02d %.2f"%(e+1,varexp1*100) + r"%")
+        #ax.set_title("Ens%02d %i"%(e+1,chk))
+    plt.tight_layout()
+    plt.savefig("%s%sPattern_PC%i_%sAvg_OLDDJFM.png" % (outpath,patname,N+1,meanname),dpi=200)
+    print("Finished PC %i" % (N+1))
+
+
 #%% Take the ensemble average, and plot one for each month
 
 # Take the ensemble average
@@ -282,7 +341,43 @@ for s in range(4):
     plt.savefig("%sSLPPattern_Ensavg_EOF23_%s.png"%(outpath,seasonname[s]),dpi=200)
 
 
+#%% Plot Forcing and SLP contours together
+
+m = 0
+N = 0
+cint1 = np.arange(-0.8,0.9,0.1)
+mons = [[11,0,1],[2,3,4],[5,6,7],[8,9,10]]
+inpc=[0,1,2]
+cmap = cmocean.cm.balance
+cmap.set_bad(color='yellow')
+cmap.set_under(color='yellow')
+cint2 = np.arange(-6,7,1)
 
 
+for iN in range(3):
+    fig,axs = plt.subplots(4,3,subplot_kw={'projection':ccrs.PlateCarree()},figsize=(12,8))
+    N = inpc[iN]
+    for s in range(4):
+        mns = mons[s]
+        for im in range(3):
+            
+            m = mns[im]
+            ax = axs[s,im]
+    
+            varc = np.copy(eof[:,m,:,:,N].mean(0))
+            varf = np.copy(pattern[:,m,:,:,N].mean(0))
+            vare = np.copy(varexp[:,m,N].mean(0))
+            varf,lon1 = add_cyclic_point(varf,lon) 
 
-
+            ax  = viz.init_map(bbox,ax=ax)
+            ctf = ax.contourf(lon1,lat,varf,cint2,cmap=cmap)
+            ax  = viz.plot_contoursign(varc,lon,lat,cint=cint1,ax=ax,add_cyc=True)
+            ax.set_title("Mon%i %.1f"%(m+1,vare*100)+r"%")
+            plt.colorbar(ctf,ax=ax,pad=0.10,fraction=0.046)
+            
+        #End mon loop
+    
+    # end season loop
+    plt.tight_layout()
+    plt.savefig("%sEOF%i_SLP_NHFLX_Plots_Ensavg.png" % (outpath,N+1),dpi=200)
+        
