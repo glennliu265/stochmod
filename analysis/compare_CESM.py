0473796a163138f62d24986a339a11e00b1966a0
Glenn <glenn.y.liu@gmail.com>
2020-08-31 17:14:14 -0400

    Added CESM autocorrelation plots
    Added CESM autocorrelation plots

--- analysis/compare_CESM.py
+++ analysis/compare_CESM.py
@@ -133,6 +133,94 @@ ax.set_title("CESM Autocorrelation")
 
 plt.savefig(outpathfig+"Region_SST_Autocorrelation_CESM.png",dpi=200)
 
+#%% Make same autocorrelation plot but using data from CESM (Hi-Res)
+
+xlim = [0,36]
+xtks = np.arange(0,39,3)
+ylim = [-0.25,1]
+ytks = np.arange(-.25,1.25,0.25)
+
+# Load CESM1LE Verification Data
+cesmsst = np.load(projpath + "01_Data/SST_Timeseries_RegionAvg.npy",allow_pickle=True).item()
+cesmauto = np.load(projpath + "01_Data/Autocorrelation_Region.npy",allow_pickle=True).item()
+
+# Set colors
+rcolmem = [np.array([189,202,255])/255,
+           np.array([255,134,134])/255,
+           np.array([153,255,153])/255,
+           [.75,.75,.75]]
+
+fig,ax = plt.subplots(1,1,figsize = (6,4))
+
+for r in range(nregion): 
+    
+    rname = regions[r]
+    
+    for e in range(42):
+   
+        ax.plot(lags,cesmauto[r][e,:],color=rcolmem[r],alpha=0.25)
+    
+ln1 = ax.plot(lags,np.nanmean(cesmauto[0],0),color=regioncolor[0],label=regions[0])
+ln2 = ax.plot(lags,np.nanmean(cesmauto[1],0),color=regioncolor[1],label=regions[1])
+ln3 = ax.plot(lags,np.nanmean(cesmauto[2],0),color=regioncolor[2],label=regions[2])
+ln4 =  ax.plot(lags,np.nanmean(cesmauto[3],0),color=regioncolor[3],label=regions[3])
+
+
+plt.grid(True)
+ax.set_xlim(xlim)
+ax.set_xticks(xtks)
+ax.set_ylim(ylim)
+ax.set_yticks(ytks)
+lns = ln1 + ln2 + ln3 + ln4
+labs = [l.get_label() for l in lns]
+ax.legend(lns,labs,prop={'size':12})
+ax.set_title("CESM1LE Autocorrelation (1920-2005")
+
+plt.savefig(outpathfig+"Region_SST_Autocorrelation_CESM_HiRes.png",dpi=200)
+
+#%% Autocorrelation Plot, but just for the NAT
+
+xlim = [0,36]
+xtks = np.arange(0,39,3)
+ylim = [-0.4,1]
+ytks = np.arange(-.25,1.25,0.25)
+
+# Load CESM1LE Verification Data
+cesmsst = np.load(projpath + "01_Data/SST_Timeseries_RegionAvg.npy",allow_pickle=True).item()
+cesmauto = np.load(projpath + "01_Data/Autocorrelation_Region.npy",allow_pickle=True).item()
+
+# Set colors
+rcolmem = [np.array([189,202,255])/255,
+           np.array([255,134,134])/255,
+           np.array([153,255,153])/255,
+           [.75,.75,.75]]
+
+fig,ax = plt.subplots(1,1,figsize = (6,4))
+
+r = 3
+    
+rname = regions[r]
+
+# for e in range(42):
+   
+#     ax.plot(lags,cesmauto[r][e,:],color=rcolmem[r],alpha=0.25)
+    
+# ln4 =  ax.plot(lags,np.nanmean(cesmauto[3],0),color=regioncolor[3],label=regions[3])
+
+ax = viz.ensemble_plot(cesmauto[3],0,ax=ax,color='k',ysymmetric=0,ialpha=0.10)
+
+plt.grid(True)
+ax.set_xlim(xlim)
+ax.set_xticks(xtks)
+ax.set_ylim(ylim)
+ax.set_yticks(ytks)
+# lns = ln3
+# labs = [l.get_label() for l in lns]
+# ax.legend(lns,labs,prop={'size':12})
+ax.set_title("CESM1LE Autocorrelation, NAT (1920-2005")
+
+plt.savefig(outpathfig+"NAT_SST_Autocorrelation_CESM_HiRes.png",dpi=200)
+
 #%% Make a referece CESM plot of region-averaged SST time series
 
 units = 'degC'
