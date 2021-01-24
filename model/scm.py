5077e236365ccbce7f36e99d399d44ed30a9ef75
Glenn <glenn.y.liu@gmail.com>
2021-01-24 16:23:42 -0500

    Add entrain parallel, remove indexing hackneyed fix
    - Add entrain parallel function (not working properly still slower)
    
    -Removed edit to start calculating autocorrelation for entrain model at feb (function has been corrected)

--- model/scm.py
+++ model/scm.py
@@ -673,6 +673,145 @@ def entrain(t_end,lbd,T0,F,beta,h,kprev,FAC,multFAC=1,debug=False,debugprint=Fal
         return temp_ts,damp_ts,noise_ts,entrain_ts,Td_ts
     return temp_ts
 
+# Entrain Model (Single Point)
+def entrain_parallel(inputs):
+    """
+    SST Stochastic Model, with Entrainment rewritten to take all inputs as 1
+    t_end,lbd,T0,F,beta,h,kprev,FAC,multFAC = inputs
+    
+    Parameters
+    ----------
+    t_end : INT
+        Length of integration, in months
+    lbd : ARRAY [12,]
+        Heat Flux Feedback (degC/sec)
+    T0 : INT
+        Initial Temperature (degC)
+    F : ARRAY [t_end,]
+        Forcing term (white noise time series) (degC/sec)
+    beta : ARRAY [12,]
+        Entrainment term coefficient (log(h(t+1)/h(t)))
+    h : ARRAY [12,]
+        Mixed Layer Depth (meters)
+    kprev : ARRAY [12,]
+        Month of detrainment (calculated through find_kprev)
+    FAC : ARRAY [12,]
+        Integration Factor ((1-exp(-lbd))/lbd)
+    multFAC : BOOL, optional
+        Set to true to apply integration factor to forcing and entrain term. 
+        The default is TRUE.
+    debug : BOOL, optional
+        Set to true to output each term separately
+    debugprint : BOOL, optional
+        Set to true to print messages at each timestep. The default is False.
+
+    Returns
+    -------
+    temp_ts : ARRAY [t_end,]
+        Resultant Temperature timeseries
+    if debug is True, returns the follow ARRAYs [t_end,]
+        damp_ts    - Damping term
+        noise_ts   - Noise term
+        entrain_ts - Entrainment Term
+        Td_ts      - Entraining Temperature
+        
+    """
+    t_end,lbd,T0,F,beta,h,kprev,FAC,multFAC = inputs
+    debug=False
+    debugprint=False
+    
+    # Preallocate
+    temp_ts = np.zeros(t_end)
+    
+    if debug:
+        noise_ts = np.zeros(t_end)
+        damp_ts = np.zeros(t_end)
+        entrain_ts = np.zeros(t_end)
+        Td_ts   = np.zeros(t_end)
+        
+    entrain_term = np.zeros(t_end)
+    
+    # Prepare the entrainment term
+    explbd = np.exp(np.copy(-lbd))
+    explbd[explbd==1] = 0
+    
+    Td0 = None # Set Td=None Initially
+    # Loop for integration period (indexing convention from matlab)
+    for t in range(t_end):
+        
+        # Get the month (start from Jan, so +1)
+        m  = (t+1)%12
+        if m == 0:
+            m = 12
+        
+        # --------------------------
+        # Calculate entrainment term
+        # --------------------------
+        if t<12: # Start Entrainment term after first 12 months
+            entrain_term = 0
+        else:
+            if beta[m-1] == 0: # For months with no entrainment
+                entrain_term = 0
+                Td0 = None # Reset Td0 term
+                if debugprint:
+                    print("No entrainment on month %i"%m)
+                    print("--------------------\n")
+            else:
+                if (Td0 is None) & (h.argmin()==m-2) :# For first entraining month
+                    Td1 = calc_Td(t,kprev,temp_ts,prevmon=False,debug=debugprint)
+                    Td0 = Td1 # Previous month does not have entrainment!
+                if Td0 is None: # Calculate Td0 
+                    Td1,Td0 = calc_Td(t,kprev,temp_ts,prevmon=True,debug=debugprint)
+                else: # Use Td0 from last timestep
+                    Td1 = calc_Td(t,kprev,temp_ts,prevmon=False,debug=debugprint)
+                
+                Td = (Td1+Td0)/2
+                if debugprint:
+                    print("Td is %.2f, which is average of Td1=%.2f, Td0=%.2f"%(Td,Td1,Td0)) 
+                    print("--------------------\n")
+                Td0 = np.copy(Td1)# Copy Td1 to Td0 for the next loop
+                
+                # Calculate entrainment term
+                entrain_term = beta[m-1]*Td
+        
+        # ----------------------
+        # Get Noise/Forcing Term
+        # ----------------------
+        noise_term = F[t]
+        
+        # ----------------------
+        # Calculate damping term
+        # ----------------------
+        if t == 0:
+            damp_term = explbd[m-1]*T0
+        else:
+            damp_term = explbd[m-1]*temp_ts[t-1]
+        
+        # ------------------------
+        # Check Integration Factor
+        # ------------------------
+        if multFAC:
+            integration_factor = FAC[m-1]
+        else:
+            integration_factor = 1
+        
+        # -----------------------
+        # Compute the temperature
+        # -----------------------
+        temp_ts[t] = damp_term + (noise_term + entrain_term) * integration_factor
+
+        # ----------------------------------
+        # Save other variables in debug mode
+        # ----------------------------------
+        if debug:
+            damp_ts[t] = damp_term
+            noise_ts[t] = noise_term * integration_factor
+            entrain_ts[t] = entrain_term * integration_factor
+    if debug:
+        return temp_ts,damp_ts,noise_ts,entrain_ts,Td_ts
+    return temp_ts
+
+
 def noentrain_2d(randts,lbd,T0,F,FAC,multFAC=1,debug=False):
     
     """
@@ -836,9 +975,10 @@ def postprocess_stochoutput(expid,datpath,rawpath,outpathdat,lags,returnresults=
             # Take regional average 
             tsmodel = np.nanmean(tsmodel,(0,1))
             
-            # Temp FIX
-            if model == 3:
-                tsmodel = np.roll(tsmodel,-1) # First t is feb.
+            # Commented out below because now first t is Jan.
+            ## Temp FIX
+            # if model == 3:
+            #     tsmodel = np.roll(tsmodel,-1) # First t is feb.
             
             sstavg[model] = np.copy(tsmodel)
             tsmodel = proc.year2mon(tsmodel) # mon x year
