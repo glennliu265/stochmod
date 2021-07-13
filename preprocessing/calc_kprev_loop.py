ecbda909b5ddb812ecbce5331c85e374eb1f22c5
Glenn Liu <glenn.y.liu@gmail.com>
2021-07-13 14:09:29 -0400

    Update to run on stormtrack

--- preprocessing/calc_kprev_loop.py
+++ preprocessing/calc_kprev_loop.py
@@ -29,17 +29,17 @@ if stormtrack == 0:
 
 
 elif stormtrack == 1: # NOTE... need to edit this
-    datpath = "/stormtrack/data3/glliu/01_Data/02_AMV_Project/02_stochmod/Model_Data/"
+    
+    datpath = "/stormtrack/data3/glliu/01_Data/02_AMV_Project/02_stochmod/HMXL/"
     sys.path.append("/home/glliu/00_Scripts/01_Projects/00_Commons/")
     sys.path.append("/home/glliu/00_Scripts/01_Projects/01_AMV/02_stochmod/stochmod/model/")
 
-import 
+import scm
 #%% User Edits
 
 #datpath = "/Users/gyl/Downloads/02_Research/01_Projects/01_AMV/02_stochmod/01_Data/model_input"
 fname   = "HMXL_PIC.nc"
 #figpath = "/Users/gyl/Downloads/02_Research/01_Projects/01_AMV/02_stochmod/02_Figures/20210715/"
-
 
 entrain1 = -1 # Value to assign for the first mixing
 entrain0 = 0  # Value when there is no entrainment
@@ -54,6 +54,8 @@ hmxl  = ds.HMXL.values
 times = ds.time.values
 print("Loaded data in %.2fs"% (time.time()-st))
 
+ds.close()
+
 #%% Calculate Kprev for each point
 st    = time.time()
 # Preallocate
@@ -65,9 +67,9 @@ for o in tqdm.tqdm(range(nlon)): # loop lon
     for a in range(nlat): # loop lat
         hpt = hmxl[o,a,:] # get point
         if np.any(np.isnan(hpt)): # Skip Land/Ice Points
-            continue        
+            continue
+        break
         kprev = scm.calc_kprev_lin(hpt,entrain1=entrain1,entrain0=entrain0)
-        
         kprevall[o,a,:] = kprev
 print("Calculated kprev in %.2fs"% (time.time()-st))               
         
