cb69677c83450f371c6f19920d7951acc00da4c9
Glenn <glenn.y.liu@gmail.com>
2021-08-06 19:35:13 -0400

    Added spectra and regional variance plots
    - Loop for forcing ID, with runid changing accordingly
    - Add lines to plot variance increase by EOF for each region
    - Do spectral analysis for AMV Index

--- analysis/sm_analysis_rewrite.py
+++ analysis/sm_analysis_rewrite.py
@@ -39,6 +39,7 @@ elif stormtrack == 1:
 
 from amv import proc,viz
 import scm
+import tbx
 
 #%% User Edits
 
@@ -48,7 +49,7 @@ lags = np.arange(0,37,1)
 # Options to determine the experiment ID
 mconfig   = "SLAB_PIC"
 nyrs      = 1000        # Number of years to integrate over
-runid     = "002"
+runid     = "003"
 
 
 # Analysis (7/26/2021, comparing 80% variance threshold and 5 or 3 EOFs)
@@ -57,7 +58,7 @@ runid     = "002"
 
 
 # Analysis: Trying different number of EOFs
-neofs       = [1,2,3,5,10]
+neofs       = [1,2,3,5,10,25,50]
 fnames      = ["flxeof_%ieofs_SLAB-PIC" % x for x in neofs]
 frcnamelong = ["%i EOFs" % x for x in neofs]
 
@@ -85,10 +86,10 @@ modelnames  = ("Constant h","Vary h","Entraining")
 
 
 # -- Select Experiment -- 
-fid   = 4
+fid   = 6
 frcname = fnames[fid]
 expid = "forcing%s_%iyr_run%s" % (frcname,nyrs,runid) 
-regid = 0
+regid = 3
 
 # Load lat/lon regional
 lon = np.load(datpath+"lon.npy")
@@ -150,9 +151,10 @@ for p in range(len(amvpat)):
 
 # Load in SSTs for each region
 sstdicts = []
+runids = ["002","002","002","002","002","003","003"] # Accomodate different RunIDs
 for f in range(len(neofs)):
     # Load the dictionary
-    expid = "forcing%s_%iyr_run%s" % (fnames[f],nyrs,runid) # Get experiment name
+    expid = "forcing%s_%iyr_run%s" % (fnames[f],nyrs,runids[f]) # Get experiment name
     rsst_fn = "%s/proc/SST_RegionAvg_%s.npy" % (datpath,expid)
     sst = np.load(rsst_fn,allow_pickle=True).item()
     sstdicts.append(sst)
@@ -172,17 +174,143 @@ for fid in range(len(fnames)):
             sstall[fid,rid,model,:] = sstin.copy()
             sstvars[fid,rid,model]   = sstvar
 
-# Make a plot of the variance... compare for each forcing scenario
-elabels = ["%i EOFs"% x for x in neofs]
+
+#%% Load corresponding CESM Data
+
+expid      = "CESM1-PIC"
+rsst_fn    = "%s/proc/SST_RegionAvg_%s.npy" % (datpath,expid)
+sstcesm    = np.load(rsst_fn,allow_pickle=True).item()
+cesmname   =  ["CESM-FULL","CESM-SLAB"]
+
+
+# Identify the variance for each region and load in numpy arrays
+#sstallcesm  = np.zeros((len(regions),2,nyrs*12)) # Forcing x Region x Model x Time
+sstvarscesm = np.zeros((len(regions),2)) # Forcing x Region x Model
+
 for rid in range(len(regions)):
-    fig,ax  = plt.subplots(1,1)
+    for model in range(len(cesmname)):
+        sstin  = sstcesm[rid][model]
+        sstvar = np.var(sstin)
+        print("Variance for region %s, model %s is %f" % (regions[rid],cesmname[model],sstvar))
+        sstvarscesm[rid,model]   = sstvar
+        
+#%% Make a plot of the variance... compare for each forcing scenario
+elabels = ["%i EOFs"% x for x in neofs]
+eofaxis = np.arange(0,51,1)
+xtk = [1,2,3,5,10,25,50]
+#xtkplot = 
+for rid in range(len(regions)):
+    fig,ax  = plt.subplots(1,1,figsize=(8,4))
     for model in range(len(modelnames)):
-        ax.plot(np.arange(0,len(neofs),1),sstvars[:,rid,model],marker="o",label=modelnames[model])
-    ax.legend()
-    ax.set_xticks(np.arange(0,len(neofs),1))
-    ax.set_xticklabels(elabels)
+        ax.plot(xtk,sstvars[:,rid,model],marker="o",label=modelnames[model])
+    
+    ax.axhline(sstvarscesm[rid,0],color="k",label="CESM-FULL (%.3f $degC^{2}$)"%(sstvarscesm[rid,0]),ls='dashed')
+    ax.axhline(sstvarscesm[rid,1],color="gray",label="CESM-SLAB (%.3f $degC^{2}$)" %(sstvarscesm[rid,1]),ls='dashed')
+    ax.legend(ncol=2)
+    ax.set_xticks(xtk)
+    #ax.set_xticklabels(elabels)
     ax.set_ylabel("$(\circ C)^{2}$")
     ax.set_xlabel("Number of EOF Patterns")
-    ax.set_title("%s AMV Index Variance vs. Number of EOF Patterns \n used to force the stochastic model (%s)" % (regions[rid],modelnames[model]))
+    ax.set_title("%s AMV Index Variance vs. Number of EOF Patterns \n used to force the stochastic model" % (regions[rid]))
     ax.grid(True,ls='dotted')
     plt.savefig("%svariance_vs_nEOF_by_model_runid%s_nyr%i_region%s.png"%(figpath,runid,nyrs,regions[rid]),dpi=150)
+
+#%% Do some spectral analysis
+
+nsmooth = 5
+pct     = 0.10
+
+rid = 3
+fid = 0
+
+dofs = [1000,1000,898,1798] # In number of years
+
+
+# Unpack and stack data
+insst = []
+for model in range(len(modelnames)):
+    insst.append(sstall[fid,rid,model,:]) # Append each stochastic model result
+insst.append(sstcesm[rid][0]) # Append CESM-FULL
+insst.append(sstcesm[rid][1]) # Append CESM-SLAB 
+
+# Calculate Spectra and confidence Intervals
+specs,freqs,CCs,dofs,r1s = scm.quick_spectrum(insst,nsmooth,pct)
+alpha = 0.05
+bnds = []
+for nu in dofs:
+    lower,upper = tbx.confid(alpha,nu*2)
+    bnds.append([lower,upper])
+    
+#%% Make the plot
+timemax = None
+xlms = [0,0.2]
+xtks = [0,0.02,0.04,0.1,0.2]
+xtkl = 1/np.array(xtks)
+dt   = 3600*24*30
+
+specnames  = np.hstack([modelnames,cesmname])
+speclabels = ["%s (%.3f $degC^2$)" % (specnames[i],np.var(insst[i])) for i in range(len(insst)) ]
+
+if timemax is None:
+    timemax = 0
+
+def lin_quickformat(ax,plotdt,freq):
+    # Set tickparams and clone
+    xtick = np.arange(0,1.7,.2)
+    ax.set_xticks(xtick)
+    ax.set_ylabel("Power ($\degree C^{2} / cpy$)",fontsize=12)
+    ax.set_xlabel("Frequency (cycles/year)",fontsize=12)
+    htax = viz.twin_freqaxis(ax,freq,"Years",dt,mode='lin-lin',xtick=xtick)
+    
+    # Set xtick labels
+    xtkl = ["%.1f" % (1/x) for x in xtick]
+    htax.set_xticklabels(xtkl)
+    
+    
+    # Set some key lines
+    ax = viz.add_yrlines(ax,dt=plotdt)
+    
+    ax.legend(fontsize=10)
+    return ax,htax
+
+
+speccolors = ["b","r","m","k","gray"]
+
+plotdt = 3600*24*365
+freq   = freqs[0]
+
+fig,ax = plt.subplots(1,1,figsize=(6,4))
+
+for i in range(len(specs)):
+    ax.plot(freqs[i]*plotdt,specs[i]/plotdt,label=speclabels[i],color=speccolors[i])
+    ax.plot(freqs[i]*plotdt,CCs[i][:,1]/plotdt,label="",color=speccolors[i],ls='dashed')
+    
+ax.legend()
+
+
+
+#i = 1
+#ax.plot(freqcesmslab*plotdt,Pcesmslab/plotdt,color='gray',label="CESM1 SLAB" + "$\; (\sigma=%.2f ^{\circ}C$)"%(np.std(nassti[-1])))
+#ax.plot(freqcesmslab*plotdt,CLs[1][:,1]/plotdt,color='gray',label="CESM1 SLAB AR1 95% Significance",ls='dashed')
+#ax.plot(freqcesmslab*plotdt,CLs[1][:,0]/plotdt,color='gray',label="CESM1 SLAB AR1",ls=':')
+#ax.plot(freqcesmfull*plotdt,Pcesmfull/plotdt,color='black',label="CESM1 FULL" + "$\; (\sigma=%.2f ^{\circ}C$)"%(np.std(nassti[0])))
+#ax.plot(freqcesmfull*plotdt,CLs[0][:,1]/plotdt,color='black',label="CESM1 FULL AR1 95% Significance",ls='dashed')
+#ax.plot(freqcesmfull*plotdt,CLs[0][:,0]/plotdt,color='black',label="CESM1 FULL AR1",ls=':')
+#ax,htax = lin_quickformat(ax,plotdt,freqcesmfull)
+#ax.set_xlabel("")
+#ax.set_title("CESM1 NASSTI (SLAB vs. FULL) \n nsmooth=%i"%(nsmooths[0]))
+
+ax.set_xlim(xlms)
+ax.set_xticks(xtks)
+xtick=np.array(xtks)
+htax = viz.twin_freqaxis(ax,freq,"Years",dt,mode='lin-lin',xtick=xtick)
+# Set xtick labels
+htax.set_xticklabels(xtkl)
+#ax.set_ylim([0,2])
+ax.legend(fontsize=10)
+plt.tight_layout()
+plt.savefig("%sPreliminNATSpec"%figpath,dpi=200)
+
+
+
+
