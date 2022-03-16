890c0e8a932dc4ba551b9bec141e1d21091b6ba4
Glenn <glenn.y.liu@gmail.com>
2022-03-16 01:23:13 -0400

    Calculate spectra at each point, then visualize ratio
    Added [pointwise spectra]
    - Calculate power spectra estimates at each point of CESM or continuous stochastic model simulation
    - Visualize parameters and area under curve for entraining model, and contours for selected threshold
    - Plot spectra over a point
    
    Added [viz_spectra_ratio]
    - Visualize spectral ratios using output saved from pointwise spectra

---
+++ analysis/pointwise_spectra.py
@@ -0,0 +1,451 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Compute the spectra for a given dataset at each point
+This is done for stochastic model output, or CESM1 preprocessed to match this
+format
+
+The first section calculates pointwise spectra
+The second section investigates the spectra over specific regions
+
+Created on Tue Mar  8 11:25:44 2022
+
+@author: gliu
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+sys.path.append("/Users/gliu/Downloads/02_Research/01_Projects/01_AMV/00_Commons/03_Scripts/")
+sys.path.append("/Users/gliu/Downloads/02_Research/01_Projects/01_AMV/02_stochmod/03_Scripts/stochmod/model/")
+from amv import proc,viz
+import scm
+
+import numpy as np
+import xarray as xr
+from tqdm import tqdm 
+import time
+
+import cartopy.crs as ccrs
+#%%
+
+# Spectra Parametsrs
+pct        = 0.10
+nsmooth    = 30
+smoothname = "smooth%03i-taper%03i" % (nsmooth,pct*100)
+
+
+
+figpath = '/Users/gliu/Downloads/02_Research/01_Projects/01_AMV/02_stochmod/02_Figures/20220305/'
+proc.makedir(figpath)
+
+#sst_fn = "stoch_output_forcingflxeof_090pct_SLAB-PIC_eofcorr2_1000yr_run200_ampq3_method5_dmp0.npz"
+#sstname = "CESM_FULL_PIC"
+#sstname  = "stoch_output_forcingflxeof_090pct_SLAB-PIC_eofcorr2_Fprime_rolln0_1000yr_run200_ampq3_method5_dmp0"
+
+
+# Postprocess Continuous SM  Run
+# ------------------------------
+datpath  = "/Users/gliu/Downloads/02_Research/01_Projects/01_AMV/02_stochmod/01_Data/model_output/"
+outpath  = "/Users/gliu/Downloads/02_Research/01_Projects/01_AMV/02_stochmod/01_Data/model_output/proc/"
+fnames   = ["forcingflxeof_090pct_SLAB-PIC_eofcorr2_Fprime_rolln0_1000yr_run2%02d_ampq0_method5_dmp0"%i for i in range(10)]
+snames     = ["spectra_%s_Fprime_rolln0_ampq0_method5_dmp0_run2%02d.nc" % (smoothname,i) for i in range(10)]
+mnames     = ["constant h","vary h","entraining"] 
+
+# Postproess Continuous CESM Run
+# ------------------------------
+datpath    = "/Users/gliu/Downloads/02_Research/01_Projects/01_AMV/02_stochmod/01_Data/"
+outpath    = "/Users/gliu/Downloads/02_Research/01_Projects/01_AMV/02_stochmod/01_Data/model_output/proc/"
+fnames     = ["CESM1_FULL_postprocessed_NAtl.nc","CESM1_SLAB_postprocessed_NAtl.nc"]
+mnames     = ["FULL","SLAB"] 
+snames     = ["spectra_%s_PIC-%s.nc" % (smoothname,mnames[i]) for i in range(2)]
+
+
+
+
+
+# Other Params
+bboxplot = [-80,0,0,60]
+mons3    = [viz.return_mon_label(m,nletters=3) for m in np.arange(1,13)]
+
+
+#%% Compute the spectra
+
+for f,sst_fn in enumerate(fnames):
+    
+    # Load in SST [model x lon x lat x time] Depending on the file format
+    if 'npy' in sst_fn:
+        print("Loading .npy")
+        sst = np.load(datpath+sst_fn)
+        # NOTE: Need to write lat/lon loader
+    elif 'npz' in sst_fn:
+        print("Loading .npz")
+        ld  = np.load(datpath+sst_fn,allow_pickle=True)
+        lon = ld['lon']
+        lat = ld['lat']
+        sst = ld['sst'] # [model x lon x lat x time]
+    elif 'nc' in sst_fn:
+        print("Loading netCDF")
+        ds  = xr.open_dataset(datpath+sst_fn)
+        
+        ds  = ds.sel(lon=slice(-80,0),lat=slice(0,65))
+        
+        
+        lon = ds.lon.values
+        lat = ds.lat.values
+        sst = ds.SST.values
+    
+    # Check dimensions
+    if len(sst.shape) < 4:
+        print("Dimensions Less than 4. Adding Extra at Start")
+        sst = sst[None,...]
+    nmod,nlon,nlat,ntime = sst.shape
+    
+    # Compute Size of Frequency (take from yo_spec)
+    fmax  = 0.5
+    df    = 1/ntime
+    freq  = np.arange(1/ntime,fmax+df,df)
+    nfreq = len(freq)
+    
+    # Preallocate 
+    specs = np.zeros((nmod,nlon,nlat,nfreq))*np.nan 
+    dofs  = np.zeros((nmod,nlon,nlat,nfreq))*np.nan # Not sure if I want to save all of this
+    r1s   = np.zeros((nmod,nlon,nlat,nfreq))*np.nan 
+    
+    # Compute the spectra (Pointwise)
+    for mid in range(nmod):
+        for o in tqdm(range(nlon)): # Loop for longitude
+            
+            for a in range(nlat): # Loop for latitude
+                
+                # Get Input
+                inssts   = [sst[mid,o,a,:],]
+                nsmooths = [nsmooth,]
+                
+                # Skip if NaNs are Present 
+                if np.any(np.isnan(sst[mid,o,a,:])):
+                    continue
+                
+                # Compute Spectra and save
+                spec,freqs,_,dof,r1 = scm.quick_spectrum(inssts,nsmooths,pct)
+                specs[mid,o,a,:] = spec[0].copy()
+                dofs[mid,o,a,:]  = dof[0].copy()
+                r1s[mid,o,a,:]   = r1[0].copy()
+                
+    # Now work on saving it
+    if "CESM" in sst_fn:
+        mname_in = [mnames[f],]
+    else:
+        mname_in  = mnames
+    dims = {'models'          : mname_in,
+            'lon'             : lon,
+            'lat'             : lat,
+            'frequency'       : freqs[0]
+            }
+    
+    das = []
+    varnames = ("spectra","dofs","r1s")
+    outvars  = [specs,dofs,r1s]
+    for v,name in enumerate(varnames):
+        da = xr.DataArray(outvars[v],
+                    dims=dims,
+                    coords=dims,
+                    name = name,
+                    )
+        if v == 0:
+            ds = da.to_dataset() # Convert to dataset
+        else:
+            ds = ds.merge(da) # Merge other datasets
+            
+        # Append to list if I want to save separate dataarrays
+        das.append(ds)
+        
+    #% Save as netCDF
+    # ---------------
+    #st = time.time()
+    encoding_dict = {name : {'zlib': True} for name in varnames} 
+    savename = outpath+snames[f]
+    print("Saving as " + savename)
+    ds.to_netcdf(savename,
+             encoding=encoding_dict)
+    #print("Saved in %.2fs" % (time.time()-st))
+# ------------------
+#%% Analysis Section
+# ------------------
+# Currently supports analysis of continuous data
+debug = True
+
+# Find Frequency (lowerthres <= f <= thresval)
+thresval   = 1/20 # 20 years (Multidecadal)
+lowerthres = 0 # 
+dtplot     = 3600*24*365
+
+
+# Load the data (Just the Spectra)
+snames_full = [outpath+sname for sname in snames]
+if "PIC" in snames_full[0]:
+    dsall = []
+    for i in range(2):
+        ds       = xr.open_mfdataset(snames_full[i])
+        dsall.append(ds)
+        
+    
+    # Load Spectra
+    st = time.time()
+    cfreqs       = []
+    cspecs       = []
+    modelnames   = []
+    for i in range(2):
+        cspecs.append(dsall[i].spectra.values)
+        cfreqs.append(dsall[i].frequency.values)
+        modelnames.append(dsall[i].models.values)
+    #specs       = ds.spectra.values # [model x lon x lat x freq]
+    
+    #specsds     = dsmean.spectra
+    print("Loaded data in %.2fs" % (st-time.time()))
+        
+else:
+    dsall       = xr.open_mfdataset(snames_full
+                                    ,combine="nested",concat_dim="run")
+
+
+
+    # Take mean of all runs
+    st = time.time()
+    dsmean      = dsall.mean('run')
+    freq        = dsmean.frequency.values
+    specs       = dsmean.spectra.values # [model x lon x lat x freq]
+    modelnames  = dsmean.models.values
+    #specsds     = dsmean.spectra
+    print("Loaded data in %.2fs" % (st-time.time()))
+
+
+if debug:
+    tids = proc.calc_specvar(freq,specs,thresval,dtplot,
+                             return_thresids=True,lowerthres=lowerthres)
+    plt.plot(freq*dtplot,tids),plt.xlim([0,1])
+
+
+# Compute Spectra sum under selected frequencies
+specsum = proc.calc_specvar(freq,specs,thresval,dtplot,lowerthres=lowerthres)
+
+#%% Plot Spectra for a point
+
+
+klon,klat = proc.find_latlon(-30,50,lon,lat)
+
+fig,ax = plt.subplots(1,1)
+for im in range(3):
+    ax.plot(freq*dtplot,specs[im,klon,klat,:]/dtplot,label=modelnames[im])
+
+ax.set_xlim([0,0.5])
+ax.axvline(thresval,color="k")
+
+#ax.axhline(0)
+ax.legend()
+
+
+
+#%% Part 1, Lets see the role of entrainment
+
+
+# Find Frequency (lowerthres <= f <= thresval)
+#thresval   = 1/5 # 20 years (Multidecadal)
+
+periods    = [2,3,4,5,6,7,8,9,10,15,20,30,40,50]
+thresvals  = [1/x for x in periods]
+lowerthres = 0 # 
+dtplot     = 3600*24*365
+
+plotentrain  = True # False to examine effect of adding MLD variations
+
+
+for thresval in tqdm(thresvals):
+    
+    # Compute Spectra sum under selected frequencies
+    specsum = proc.calc_specvar(freq,specs,thresval,dtplot,lowerthres=lowerthres)
+    
+    # Difference in variance < 1/20 years
+    if plotentrain:
+        entraindiff = specsum[2,...] - specsum[1,...]
+        plotvar     = entraindiff
+        vname       = "Entrain_VaryH"
+        vname_title = "$\sigma^2_{entrain}$ - $\sigma^2_{vary \, h}$"
+    else:
+        mlddiff     = specsum[1,...] - specsum[0,...]
+        plotvar     = mlddiff
+        vname       = "VaryH_ConstantH"
+        vname_title = "$\sigma^2_{vary \, h}$ - $\sigma^2_{constant \, h}$"
+    
+    
+    cint        = np.arange(-.1,.11,0.01)
+    fig,ax      = plt.subplots(1,1,constrained_layout=True,
+                               subplot_kw={'projection':ccrs.PlateCarree()})
+    ax          = viz.add_coast_grid(ax,bbox=bboxplot,fill_color='gray')
+    #pcm        = ax.pcolormesh(lon,lat,entraindiff.T,cmap='cmo.balance',vmin=-.15,vmax=.15)
+    pcm         = ax.contourf(lon,lat,plotvar.T,cmap='cmo.balance',levels=cint,extend='both')
+    cl          = ax.contour(lon,lat,plotvar.T,levels=[0,1],colors="k",lw=1.5)
+    ax.clabel(cl)
+    cb = fig.colorbar(pcm,ax=ax)
+    cb.set_label("SST Variance Difference ($K^2$)")
+    ax.set_title("%s @ Frequencies < %i years$^{-1}$"% (vname_title,1/thresval))
+    
+    savename = "%s%s_Difference_period%02d.png" %  (figpath,vname,1/thresval)
+    plt.savefig(savename,dpi=150)
+
+#%% Load Stochastic Model Inputs
+
+# Use the function used for sm_rewrite.py
+frcname    = "flxeof_090pct_SLAB-PIC_eofcorr2_Fprime_rolln0"
+input_path = datpath + "../model_input/"
+lagstr     = "lag1"
+method     = 5 # Damping Correction Method
+inputs = scm.load_inputs('SLAB_PIC',frcname,input_path,load_both=True,method=method,lagstr=lagstr)
+long,latg,h,kprevall,dampingslab,dampingfull,alpha,alpha_full = inputs
+hblt   = np.load(input_path + "SLAB_PIC_hblt.npy") # Slab fixed MLD
+hblt   = np.ones(hblt.shape) * hblt.mean(2)[:,:,None]
+
+# Compute Specific Region
+reg_sel  = [lon[0],lon[-1],lat[0],lat[-1]]
+#reg_sel   = [-80,0,30,65]
+inputs = [h,kprevall,dampingslab,dampingfull,alpha,alpha_full,hblt]
+outputs,lonr2,latr2 = scm.cut_regions(inputs,long,latg,reg_sel,0)
+h,kprev,damping,dampingfull,alpha,alpha_full,hblt = outputs
+
+npts = len(lon) * len(lat)
+#%% Next, Try to Plot a given threshold
+
+bboxinset = [-80,0,30,60]
+# Compute Spectra sum under selected frequencies
+thresval    = 1/20 # 1/year
+lowerthres  = 0 # 
+dtplot      = 3600*24*365
+specsum     = proc.calc_specvar(freq,specs,thresval,dtplot,lowerthres=lowerthres)
+entraindiff = specsum[2,...] - specsum[1,...]
+plotvar = entraindiff
+
+vlm     = [-0.1,0.1]
+
+vthreses = np.arange(0.02,0.09,0.01) # [0.08,0.10,0.12,0.14,0.16,0.18,0.20]
+ylabs = ("MLD (m)",
+         "Damping ($Wm^{-2}K^{-1}$)",
+         "Forcing ($Wm^{-2}$)")
+
+palpha = 0.05
+salpha = 0.3 # Seleccted Alpha
+# Just Plot Everything
+xlims  = [0,11]
+
+for vthres_sel in tqdm(vthreses):
+
+    kthres     = plotvar > vthres_sel
+    kthres     = kthres.flatten()
+    
+    fig,axs = plt.subplots(4,1,figsize=(8,8),constrained_layout=True,sharex=False)
+    
+    # Plot MLD
+    ax = axs[0]
+    mlins = ax.plot(mons3,h.reshape(npts,12).T,alpha=palpha,color="k")
+    ax.set_ylabel(ylabs[0])
+    mlins_sel = ax.plot(mons3,h.reshape(npts,12)[kthres,:].T,alpha=salpha,color="yellow")
+    ax.set_xlim(xlims)
+    ax.grid(True,ls='dotted',alpha=0.25)
+    
+    ax = axs[1]
+    dlins = ax.plot(mons3,damping.reshape(npts,12).T,alpha=palpha,color="r")
+    dlins2 = ax.plot(mons3,dampingfull.reshape(npts,12).T,alpha=palpha,color="b")
+    ax.set_ylabel(ylabs[1])
+    dlins2_sel = ax.plot(mons3,dampingfull.reshape(npts,12)[kthres,:].T,alpha=salpha,color="yellow")
+    ax.set_xlim(xlims)
+    ax.grid(True,ls='dotted',alpha=0.25)
+    
+    ax = axs[2]
+    flins = ax.plot(mons3,np.linalg.norm(alpha,axis=2).reshape(npts,12).T,alpha=palpha,color="r")
+    flins_2 = ax.plot(mons3,np.linalg.norm(alpha_full,axis=2).reshape(npts,12).T,alpha=palpha,color="b")
+    ax.set_xlim(xlims)
+    ax.set_ylabel(ylabs[2])
+    
+    flins_sel = ax.plot(mons3,np.linalg.norm(alpha_full,axis=2).reshape(npts,12)[kthres,:].T,
+                        alpha=salpha,color="yellow")
+    ax.grid(True,ls='dotted',alpha=0.25)
+    
+    ax = axs[3]
+    ax.axis('off')
+    
+    # Add Locator
+    # These are in unitless percentages of the figure size. (0,0 is bottom left)
+    ax2 = axs[3]
+    left, bottom, width, height = [0.18, -0.45, 0.70, 1]
+    ax2 = fig.add_axes([left, bottom, width, height],projection=ccrs.PlateCarree())
+    ax2 = viz.add_coast_grid(ax2,bbox=bboxinset,fill_color='gray',ignore_error=True)
+    pcm = ax2.pcolormesh(lon,lat,plotvar.T,
+                    cmap='cmo.balance',vmin=vlm[0],vmax=vlm[1])
+    cl  = ax2.contour(lon,lat,plotvar.T,levels=[vthres_sel,],colors="k")
+    ax2.clabel(cl)
+    #viz.plot_mask(lon,lat,dmsks[mid],ax=ax2,markersize=2,color='k',marker="o")
+    fig.colorbar(pcm,ax=ax2,fraction=0.02)
+    
+    plt.suptitle("Red (SLAB), Blue (FULL), Yellow ($\sigma^2_{SST}$ >  %.2f $K^{2}$)"% (vthres_sel))
+    plt.savefig("%sEntrain_Diff_Params_thres%iyrs_thresval%.2f_wmap.png"% (figpath,int(1/thresval),vthres_sel),dpi=150,bbox_inches='tight')
+# add spectra?
+
+
+# ----------------------------------------------------------
+#%% Plot all the spectra/autocorrelation over a given region
+# ----------------------------------------------------------
+
+cid     = 0
+bbsel = [-80, -40, 20, 40]
+
+bbstr = "lon%ito%i_lat%ito%i" % (bbsel[0],bbsel[1],bbsel[2],bbsel[3])
+
+# Visualize Spectra in this region
+dsreg = dsall[cid].sel(lon=slice(bbsel[0],bbsel[1]),lat=slice(bbsel[2],bbsel[3]))
+rspec = dsreg.spectra.values
+rlon  = dsreg.lon.values
+rlat  = dsreg.lat.values
+rfreq = dsreg.frequency.values
+
+#%
+
+# Spaghetti Plot
+
+dtplot = 3600*24*365
+
+
+xtks = [1/100,1/50,1/25,1/10,1/5,1/2,1/1]
+xper = [int(1/x) for x in xtks]
+
+
+nmod,nlon,nlat,ntime = rspec.shape
+specsplot = rspec.reshape(nmod*nlon*nlat,ntime)
+
+fig,ax = plt.subplots(1,1,figsize=(10,3))
+ln1    = ax.plot(rfreq*dtplot,specsplot.T/dtplot,label="",lw=1,alpha=0.05,color='k')
+
+ax.set_xlim([xtks[0],xtks[-1]])
+ax.set_xlabel("Period (Years)")
+ax.set_ylabel("Power ($K^2 cpy^{-1}$)")
+ax.grid(True)
+
+ax2 = ax.twiny()
+ax2.set_xlim([xtks[0],xtks[-1]])
+ax2.set_xticks(xtks)
+ax2.set_xticklabels(xper,fontsize=8,rotation=45)
+ax2.grid(True,ls='dotted',color='gray')
+ax2.set_xlabel("Period (Years)")
+#ln1_se = ax.plot(freqs[0]*dtplot,specsplot[kthres,:].T/dtplot,label="",lw=1,alpha=0.5,color='yellow')
+
+left, bottom, width, height = [0.65, 0.50, 0.30, 0.35]
+ax3 = fig.add_axes([left, bottom, width, height],projection=ccrs.PlateCarree())
+ax3 = viz.add_coast_grid(ax3,bbox=bboxplot,fill_color='gray',ignore_error=True)
+ax3 = viz.plot_box(bbsel,ax=ax3,linestyle='solid',color='k')
+
+
+savename = "%sCESM-%s_Spectra_bbox_%s_%s.png" % (figpath,mnames[cid],bbstr,smoothname)
+
+plt.savefig(savename,dpi=200)
+#ax3.plot(lonr2[klon],latr2[klat],marker="x",color="k",markersize=10)
+
+
+
---
+++ analysis/viz_spectra_ratio.py
@@ -0,0 +1,221 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+
+Plot Regional Spectra, Ratio
+
+Plot spectral ratio over different regions
+
+Created on Tue Mar 15 22:05:37 2022
+
+@author: gliu
+"""
+
+import numpy as np
+import cartopy.crs as ccrs
+import matplotlib.pyplot as plt
+import sys
+import cmocean
+from tqdm import tqdm
+import xarray as xr
+import time
+#%% Set Paths, Import Custom Modules
+stormtrack = 0
+if stormtrack == 0:
+    projpath   = "/Users/gliu/Downloads/02_Research/01_Projects/01_AMV/02_stochmod/"
+    datpath     = projpath + '01_Data/model_output/'
+    rawpath     = projpath + '01_Data/model_input/'
+    outpathdat  = datpath + '/proc/'
+    figpath     = projpath + "02_Figures/20220305/"
+   
+    sys.path.append("/Users/gliu/Downloads/02_Research/01_Projects/01_AMV/02_stochmod/03_Scripts/stochmod/model/")
+    sys.path.append("/Users/gliu/Downloads/02_Research/01_Projects/01_AMV/00_Commons/03_Scripts/")
+
+elif stormtrack == 1:
+    datpath     = "/stormtrack/data3/glliu/01_Data/02_AMV_Project/02_stochmod/Model_Data/model_output/"
+    rawpath     = "/stormtrack/data3/glliu/01_Data/02_AMV_Project/02_stochmod/Model_Data/model_input/"
+    outpathdat  = datpath + '/proc/'
+    
+    
+    sys.path.append("/home/glliu/00_Scripts/01_Projects/00_Commons/")
+    sys.path.append("/home/glliu/00_Scripts/01_Projects/01_AMV/02_stochmod/stochmod/model/")
+
+from amv import proc,viz
+import scm
+import tbx
+
+proc.makedir(figpath)
+
+#%%
+
+# Spectra Parametsrs
+pct        = 0.10
+nsmooth    = 30
+smoothname = "smooth%03i-taper%03i" % (nsmooth,pct*100)
+
+outpath  = "/Users/gliu/Downloads/02_Research/01_Projects/01_AMV/02_stochmod/01_Data/model_output/proc/"
+figpath = '/Users/gliu/Downloads/02_Research/01_Projects/01_AMV/02_stochmod/02_Figures/20220315/'
+proc.makedir(figpath)
+
+#sst_fn = "stoch_output_forcingflxeof_090pct_SLAB-PIC_eofcorr2_1000yr_run200_ampq3_method5_dmp0.npz"
+#sstname = "CESM_FULL_PIC"
+#sstname  = "stoch_output_forcingflxeof_090pct_SLAB-PIC_eofcorr2_Fprime_rolln0_1000yr_run200_ampq3_method5_dmp0"
+
+
+# Postprocess Continuous SM  Run
+# ------------------------------
+fnames     = ["forcingflxeof_090pct_SLAB-PIC_eofcorr2_Fprime_rolln0_1000yr_run2%02d_ampq0_method5_dmp0"%i for i in range(10)]
+snames     = ["spectra_%s_Fprime_rolln0_ampq0_method5_dmp0_run2%02d.nc" % (smoothname,i) for i in range(10)]
+mnames     = ["constant h","vary h","entraining"] 
+snames_full = [outpath+sname for sname in snames]
+
+# Postproess CESM Run
+# ------------------------------
+cesm_fnames     = ["CESM1_FULL_postprocessed_NAtl.nc","CESM1_SLAB_postprocessed_NAtl.nc"]
+cesm_mnames     = ["FULL","SLAB"] 
+cesm_snames     = ["spectra_%s_PIC-%s.nc" % (smoothname,cesm_mnames[i]) for i in range(2)]
+cesm_snames_full = [outpath+sname for sname in cesm_snames]
+
+# Other Params
+bboxplot = [-80,0,0,60]
+mons3    = [viz.return_mon_label(m,nletters=3) for m in np.arange(1,13)]
+
+#%% Load in the spectra for each
+
+debug = True
+
+# Find Frequency (lowerthres <= f <= thresval)
+thresval   = 1/20 # 20 years (Multidecadal)
+lowerthres = 0 # 
+dtplot     = 3600*24*365
+
+
+# Load CESM
+# ---------
+cesm_dsall = []
+for i in range(2):
+    ds       = xr.open_mfdataset(cesm_snames_full[i])
+    cesm_dsall.append(ds)
+    
+    
+# Load Spectra
+st = time.time()
+cfreqs       = []
+cspecs       = []
+cmodelnames   = []
+for i in range(2):
+    cspecs.append(cesm_dsall[i].spectra.values)
+    cfreqs.append(cesm_dsall[i].frequency.values)
+    cmodelnames.append(cesm_dsall[i].models.values)
+
+    print("Loaded data in %.2fs" % (st-time.time()))
+            
+# Load Stochastic Model
+# ---------------------------
+dsall       = xr.open_mfdataset(snames_full
+                                ,combine="nested",concat_dim="run")
+
+# Take mean of all runs
+st = time.time()
+dsmean      = dsall.mean('run')
+freq        = dsmean.frequency.values
+specs       = dsmean.spectra.values # [model x lon x lat x freq]
+modelnames  = dsmean.models.values
+#specsds     = dsmean.spectra
+print("Loaded data in %.2fs" % (time.time()-st))
+
+
+#%% Plot variance thresholds over specific regions
+
+
+threses = ([0,1/20],[1/20,1/10],[1/10,1/2]) # [lower freq, upper freq]
+dtplot  = 3600*24*365
+
+inspecs = (specs[0,...],
+           specs[1,...],
+           specs[2,...],
+           cspecs[0].squeeze(),
+           cspecs[1].squeeze())
+
+infreqs = (freq,freq,freq,cfreqs[0],cfreqs[1])
+innames = mnames+cesm_mnames
+
+
+nmods,nlon,nlat,nfreqsm = specs.shape
+
+sumvals = np.zeros((nlon,nlat,len(inspecs),len(threses))) # [lon, lat x model x threshold]
+
+for t in range(len(threses)):
+    
+    
+    lowerthres,thresval = threses[t]
+    
+    for mid in range(len(inspecs)):
+        inspec = inspecs[mid]
+        infreq = infreqs[mid]
+    
+        outfreq = proc.calc_specvar(infreq,inspec,thresval,dtplot,
+                                 return_thresids=False,lowerthres=lowerthres)
+        
+        sumvals[:,:,mid,t] = outfreq.copy()
+    
+#%% Now Lets Plot the Ratios
+
+full_slab     = np.log(sumvals[:,:,3,:]/sumvals[:,:,4,:])
+entrain_hvary = np.log(sumvals[:,:,2,:]/sumvals[:,:,1,:])
+hvary_hconst  = np.log(sumvals[:,:,1,:]/sumvals[:,:,0,:])
+entrain_full  = np.log(sumvals[:,:,2,:]/sumvals[:,:,3,:])
+hconst_slab   = np.log(sumvals[:,:,0,:]/sumvals[:,:,4,:])
+#%%
+
+# ratiosel  = entrain_hvary
+# rationame = "log($\sigma^2_{entrain}$/$\sigma^2_{h vary}$)"
+# rationame_fn = "log_entrain_hvary"
+
+ratiosel     = full_slab
+rationame    = "log($\sigma^2_{full}$/$\sigma^2_{slab}$)"
+rationame_fn = "log_full_slab"
+
+ratiosel     = hvary_hconst
+rationame    = "log($\sigma^2_{h vary}$/$\sigma^2_{h const}$)"
+rationame_fn = "log_hvary_hconst"
+
+ratiosel     = entrain_full
+rationame    = "log($\sigma^2_{entrain}$/$\sigma^2_{full}$)"
+rationame_fn = "log_entrain_full"
+
+ratiosel     = hconst_slab
+rationame    = "log($\sigma^2_{h const}$/$\sigma^2_{slab}$)"
+rationame_fn = "log_hconst_slab"
+
+
+use_contours = True
+cints       = np.arange(-1.5,1.55,0.05)
+cl_ints     = np.arange(-1.5,1.6,0.1)
+fig,axs = plt.subplots(1,3,subplot_kw={'projection':ccrs.PlateCarree()},
+                       constrained_layout=True,figsize=(12,4)) 
+for t in range(3):
+    blabel = [0,0,0,1]
+    if t == 0:
+        blabel[0] = 1
+    
+    ax = axs.flatten()[t]
+    print(t)
+    if t == 0:
+        ptitle = r"> %03d Years" % (1/threses[t][1])
+    else:
+        ptitle = "%03d to %03d Years" % (1/threses[t][1],1/threses[t][0])
+    ax.set_title(ptitle)
+    ax = viz.add_coast_grid(ax,bbox=bboxplot,blabels=blabel,fill_color='gray')
+    if use_contours:
+        pcm = ax.contourf(ds.lon,ds.lat,ratiosel[:,:,t].T,levels=cints,extend='both',cmap='cmo.balance')
+        cl = ax.contour(ds.lon,ds.lat,ratiosel[:,:,t].T,levels=cl_ints,colors='k',linewidths=0.5)
+        ax.clabel(cl,cl_ints[::2],fmt="%.1f")
+    else:
+        pcm = ax.pcolormesh(ds.lon,ds.lat,ratiosel[:,:,t].T,vmin=-1.5,vmax=1.5,cmap='cmo.balance')
+
+fig.colorbar(pcm,ax=axs.flatten(),fraction=0.025,pad=0.01)
+ax = axs[0]
+ax.text(-0.15, 0.55, rationame, va='bottom', ha='center',rotation='vertical',
+        rotation_mode='anchor',transform=ax.transAxes)
+plt.savefig("%sSpectra_Ratio_%s_%s.png"% (figpath,rationame_fn,smoothname),dpi=150)
