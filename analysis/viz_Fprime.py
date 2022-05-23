79f0c0db4eec0888b2336ef4ce6409d315ca1cd5
Glenn <glenn.y.liu@gmail.com>
2022-05-23 15:49:08 -0400

    Added Qnet Spectra
    Added option to plot the Qnet spectra

--- analysis/viz_Fprime.py
+++ analysis/viz_Fprime.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 
 """
-
 Analyze the F' Variable calculated from CESM1
 
 Created on Wed May 18 17:49:05 2022
@@ -22,19 +21,22 @@ sys.path.append("/Users/gliu/Downloads/02_Research/01_Projects/01_AMV/00_Commons
 from amv import proc,viz
 import scm
 import yo_box as ybx
+
 #%%
 
 datpath  = '/Users/gliu/Downloads/02_Research/01_Projects/01_AMV/02_stochmod/01_Data/'
 fnames   = "Fprime_PIC_%s_rolln0.nc"
 mconfigs = ["FULL","SLAB"]
 
+figpath = "/Users/gliu/Downloads/02_Research/01_Projects/01_AMV/02_stochmod/02_Figures/20220526/"
+proc.makedir(figpath)
+
+fnames2 = "CESM_proc/NHFLX_PIC_%s.nc"
+
 
 #%% Load whole Dataset
 
-
-
 i      = 1
-
 ds     = xr.open_dataset(datpath+fnames % (mconfigs[i]))
 st     = time.time()
 fprime = ds.Fprime.values
@@ -50,8 +52,11 @@ lonf = 330
 latf = 50
 
 fprimes = []
+qnets   = []
+
 for i in range(2):
     
+    # Load Fprime
     ds     = xr.open_dataset(datpath+fnames % (mconfigs[i]))
     ds     = ds.sel(lon=lonf,lat=latf,method='nearest')
     st     = time.time()
@@ -60,7 +65,22 @@ for i in range(2):
     lat    = ds.lat.values
     lon    = ds.lon.values
     fprimes.append(fprime)
+    
+    ds     = xr.open_dataset(datpath+fnames2 % (mconfigs[i]))
+    ds     = ds.sel(lon=lonf,lat=latf,method='nearest')
+    
+    # Remove the mean scycle
+    ds     = ds - ds.mean('yr')
+    
+    # Load out the variables
+    st     = time.time()
+    qnet = ds.NHFLX.values.flatten()
+    lat    = ds.lat.values
+    lon    = ds.lon.values
+    qnets.append(qnet)
+    
     print("Loaded in %.2fs"%(time.time()-st))
+
     
 #%% Anomalize (or check anomalization)
 
@@ -68,6 +88,8 @@ for i in range(2):
 i = 1
 
 scycle,ts_monyr = proc.calc_clim(fprimes[i],0,returnts=1)
+
+scycleq,ts_monyr = proc.calc_clim(fprimes[i],0,returnts=1)
     
 #%% Now Do Spectra Analysis
 
@@ -118,24 +140,7 @@ def mc_wn_spectra(ts,nsamples,nsmooth,pct,return_pcts="ALL",tails="both"):
         if tails == 'lower':
             cfspec = cfspec[0,...]
         return cfspec,freq
-        
-        
-            
-            
-            
-        
-            
-        
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
 
 # Calculate Spectra
 nsmooth = 100
@@ -143,14 +148,25 @@ pct     = 0.10
 specs,freqs,CCs,dofs,r1s = scm.quick_spectrum(fprimes,nsmooth,pct)
 dtplot  = 3600*24*365
 
+qspecs,qfreqs,qCCs,qdofs,qr1s = scm.quick_spectrum(qnets,nsmooth,pct)
 
-# Compute White Noise confidence
-nsamples = 10000
+
+#%% Compute White Noise confidence
+nsamples = 1000
 pcts     = [0.025,0.05,0.5]
 cfspecs = []
 for i in tqdm(range(2)):
     cfspec,freq = mc_wn_spectra(fprimes[i],nsamples,nsmooth,pct,return_pcts=pcts,tails="both")
     cfspecs.append(cfspec)
+    
+    
+qcfspecs = []
+for i in tqdm(range(2)):
+    cfspec,freq = mc_wn_spectra(qnets[i],nsamples,nsmooth,pct,return_pcts=pcts,tails="both")
+    qcfspecs.append(cfspec)
+    
+    
+    
 
 #%%
 
@@ -162,25 +178,58 @@ xlm     = (xtk[0],xtk[-1])
 pctid   = 0
 
 
+
+pcolors  = ["red","blue",]
+pcolors2 = ["orange","violet"]
+
 # Plot Results
 fig,ax = plt.subplots(1,1)
 for mc in range(2):
-    ax.plot(freqs[mc]*dtplot,specs[mc]/dtplot,label=mconfigs[mc])
     
-    # Plot the confidence
-    ax.fill_between(freqs[mc]*dtplot,cfspecs[mc][0,pctid,:]/dtplot,
-                    cfspecs[mc][1,pctid,:]/dtplot,alpha=0.25)
-    
-ax.legend()
+    ax.plot(freqs[mc]*dtplot,specs[mc]/dtplot,label="F' " + mconfigs[mc],color=pcolors[mc],zorder=1)
 
+    ax.plot(qfreqs[mc]*dtplot,qspecs[mc]/dtplot,label="$Q'_{net}$ "+mconfigs[mc],color=pcolors2[mc],zorder=1)
+    
+    
+    for q in range(2):
+        if q == 0:
+            incfspecs = cfspecs
+            infreqs   = freqs
+            incolors  = pcolors
+        if q == 1:
+            incfspecs = qcfspecs
+            infreqs   = qfreqs
+            incolors  = pcolors2
+            
+        # Plot the confidence
+        for i in [0,2]:
+            
+            if i == 0:
+                ls = 'dotted'
+            elif i == 1:
+                ls = 'dashed'
+            elif i == 2:
+                ls = 'solid'
+                
+            if i < 2:
+                ax.fill_between(infreqs[mc]*dtplot,incfspecs[mc][0,i,:]/dtplot,
+                                incfspecs[mc][1,i,:]/dtplot,alpha=0.15,color=incolors[mc],zorder=9)
+            
+            ax.plot(infreqs[mc]*dtplot,incfspecs[mc][0,i,:]/dtplot,color=incolors[mc],ls=ls,alpha=0.3)
+            ax.plot(infreqs[mc]*dtplot,incfspecs[mc][1,i,:]/dtplot,color=incolors[mc],ls=ls,alpha=0.3)
+
+ax.legend()
 ax.set_xticks(xtk)
 ax.set_xlim(xlm)
-
 ax2 = ax.twiny()
 ax2.set_xticks(xtk)
 ax2.set_xlim(xlm)
 ax.set_xticklabels(xper)
-    
 
+ax.grid(True,ls='dotted')
+plt.savefig("%sFprime_Qnet_Spectra_Comparison.png" % (figpath),dpi=150)
+
+
+#%%
 
 #viz.plot_freqlin()
