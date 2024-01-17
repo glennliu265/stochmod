e830cdfbb06e329227af9e9df18a69288d4870bd
Glenn Liu <glenn.y.liu@gmail.com>
2024-01-17 18:24:06 -0500

    Updated loop for different forcings/damping shifts

--- analysis/test_forcing_damping_shift.py
+++ analysis/test_forcing_damping_shift.py
@@ -37,13 +37,14 @@ import yo_box as ybx
 # Path to Input Data
 input_path = '/Users/gliu/Downloads/02_Research/01_Projects/01_AMV/02_stochmod/01_Data/model_input/'
 
-figpath    = "/Users/gliu/Downloads/02_Research/01_Projects/01_AMV/02_stochmod/01_Figures/20240119"
+figpath    = "/Users/gliu/Downloads/02_Research/01_Projects/01_AMV/02_stochmod/02_Figures/20240119/"
 proc.makedir(figpath)
 
 #%% Load the Different Forcings
 
 # Forcing File Names
 fnames = [
+    
     "SLAB_PIC_NAO_EAP_NHFLX_Forcing_DJFM-MON.npy",
     "flxeof_090pct_SLAB-PIC_eofcorr2_Fprime_rolln0.npy",
     "flxeof_090pct_FULL-PIC_eofcorr2_Fprime_rolln0.npy",
@@ -201,10 +202,9 @@ acs_old = ld['acs']
 
 
 #%% Experiment with different rolls
-forcingroll = 1
+forcingroll = 0
 dampingroll = 0
-
-rollstr = "damproll%i_forceroll%i" % (dampingroll,forcingroll)
+rollstr     = "damproll%i_forceroll%i" % (dampingroll,forcingroll)
 
 #%% Integrate the stochastic Model
 
@@ -219,13 +219,12 @@ for ex in range(nexps):
     d_in = np.roll(dampings[ex].copy(),dampingroll)
     
     
-    # Set up Stochastic Model Input -------------------------------------------
+    # Set up Stochastic Model Input...
     smconfig = {}
     smconfig['eta']     = eta.copy()               # White Noise Forcing
     smconfig['forcing'] = f_in.copy()[None,None,:] # [x x 12] # Forcing Amplitude (W/m2)
     smconfig['damping'] = d_in.copy()[None,None,:] # Damping Amplitude (degC/W/m2)
     smconfig['h']       = np.ones((1,1,12)) * hblt # MLD (meters)
-    
     
     if debug: # Debug Plot of the Inputs
         fig,ax = viz.init_monplot(1,1,)
@@ -235,6 +234,7 @@ for ex in range(nexps):
         ax.legend()
         ax.set_title("Experiment %i" % (ex+1))
     
+    # Run Stochastic Model (No entrain) ---------------------------------------
     # Convert units (W/m2 to degC/S)
     smconfig['damping']=scm.convert_Wm2(smconfig['damping'],smconfig['h'],dt)[None,None,:]
     smconfig['forcing']=scm.convert_Wm2(smconfig['forcing'],smconfig['h'],dt)
@@ -245,6 +245,7 @@ for ex in range(nexps):
     # Do Integration
     output = scm.integrate_noentrain(smconfig['damping'],smconfig['Fprime'],debug=True)
     # -------------------------------------------------------------------------
+    
     outputs.append(output)
 
 
@@ -323,4 +324,133 @@ ax.legend()
 savename = "%sDebug_ACF_basemonth%i_%s.png" % (figpath,kmonth+1,rollstr)
 plt.savefig(savename,dpi=150,bbox_inches='tight')
 
-#%% Plot 
+# -----------------------------------------------------------------------------
+#%% For a given experiment, test different rolling of damping/forcing
+# -----------------------------------------------------------------------------
+
+# Selec input forcing/damping
+ex           = 0
+forcing_in   = forcings[ex]
+damping_in   = dampings[ex]
+
+# Select the forcing/damping roll amounts (amt to roll forward)
+frolls = [0,0,1,1] # forcing
+drolls = [0,1,0,1] # damping
+
+# Loop for combinations
+ncombo = len(frolls)
+routputs  = [] # [Combo][term (SST, Forcing, Damping)]
+rforcings = []
+rdampings = []
+rollnames = []
+for n in range(ncombo):
+    
+    forcingroll = frolls[n]
+    dampingroll = drolls[n]
+    rollstr     = "damproll%02i_forceroll%02i" % (dampingroll,forcingroll)
+    
+    # Get Forcing/Damping and Roll
+    f_in = np.roll(forcing_in.copy(),forcingroll)
+    d_in = np.roll(damping_in.copy(),dampingroll)
+    
+    # Copied from above =======================================================
+    # Set up Stochastic Model Input...
+    smconfig = {}
+    smconfig['eta']     = eta.copy()               # White Noise Forcing
+    smconfig['forcing'] = f_in.copy()[None,None,:] # [x x 12] # Forcing Amplitude (W/m2)
+    smconfig['damping'] = d_in.copy()[None,None,:] # Damping Amplitude (degC/W/m2)
+    smconfig['h']       = np.ones((1,1,12)) * hblt # MLD (meters)
+    
+    if debug: # Debug Plot of the Inputs
+        fig,ax = viz.init_monplot(1,1,)
+        ax.plot(mons3,smconfig['forcing'].squeeze(),label='forcing')
+        ax2 = ax.twinx()
+        ax2.plot(mons3,smconfig['damping'].squeeze(),label='damping',color='red')
+        ax.legend()
+        ax.set_title("Experiment %i" % (ex+1))
+    
+    # Run Stochastic Model (No entrain) ---------------------------------------
+    # Convert units (W/m2 to degC/S)
+    smconfig['damping']=scm.convert_Wm2(smconfig['damping'],smconfig['h'],dt)[None,None,:]
+    smconfig['forcing']=scm.convert_Wm2(smconfig['forcing'],smconfig['h'],dt)
+    
+    # Make Forcing
+    smconfig['Fprime']= np.tile(smconfig['forcing'],nyrs) * smconfig['eta'][None,None,:]
+    
+    # Do Integration
+    output = scm.integrate_noentrain(smconfig['damping'],smconfig['Fprime'],debug=True)
+    
+    #  ========================================================================
+    
+    # Append Output
+    routputs.append(output)
+    rollnames.append(rollstr)
+    
+    rforcings.append(f_in)
+    rdampings.append(d_in)
+
+# Calculate the Output
+ssts      = [o[0].squeeze() for o in routputs]
+tsmetrics = scm.compute_sm_metrics(ssts)
+
+
+#%%
+
+nc = 2
+
+for nc in range(ncombo):
+    
+    
+    ytks  = np.arange(10,80,10)
+    ytks2 = np.arange(0,36,5)
+    
+    fig,axs = viz.init_monplot(2,1,figsize=(6,5.5))
+    
+    # Plot Monthly Variance
+    ax = axs[0]
+    plotvar = tsmetrics['monvars'][nc]
+    ax.plot(mons3,plotvar,label=rollnames[nc],marker="o",color='purple')
+    
+    # Plot Base
+    plotvar = tsmetrics['monvars'][0]
+    ax.plot(mons3,plotvar,label="base",marker="o",color='purple',alpha=0.2)
+    
+    ax.plot(mons3,mvs[-1],label="SLAB",color="gray",ls="dashed")
+    ax.legend()
+    ax.set_ylim([0.5,1.5])
+    ax.set_ylabel("SST Variance (degC2)")
+    
+    
+    # --- <0> . <0> --- <0> . <0> ---
+    # Plot Forcing and Damping Shift
+    ax = axs[1]
+    
+    # Plot Forcing/Damping (Base)
+    ax.plot(mons3,rforcings[0],color="cornflowerblue",lw=2.5,label="forcing",marker="o",alpha=0.15)
+    ax2 = ax.twinx()
+    ax2.plot(mons3,rdampings[0],color="red",lw=2.5,label="damping",marker='d',ls='dashed',alpha=0.15)
+    
+    # Plot Forcing (Rolled)
+    ax.plot(mons3,rforcings[nc],color="cornflowerblue",lw=2.5,label="forcing",marker="o")
+    ax.tick_params(axis='y', colors='cornflowerblue')
+    
+    # Plot Damping (Rolled)
+    ax2.plot(mons3,rdampings[nc],color="red",lw=2.5,label="damping",marker='d',ls='dashed')
+    ax2.tick_params(axis='y', colors='red')
+    
+    # Set Limits and Ticks
+    ax.set_ylim([10,70])
+    ax.set_yticks(ytks)
+    ax.set_ylabel("Forcing (W/m2)")
+    
+    ax2.set_ylim([0,35])
+    ax2.set_yticks(ytks2)
+    ax2.set_ylabel("Damping (degC/W/m2)")
+    # Subplot Title
+    # viz.label_sp(expnames[ff],x=0.45,ax=ax,labelstyle="%s",usenumber=True)
+    
+    plt.suptitle("Damping Shift (%i) | Forcing Shift (%i)" % (drolls[nc], frolls[nc]))
+    
+    
+    savename = "%s/Debug_Monvar_%s.png" % (figpath,rollnames[nc])
+    plt.savefig(savename,dpi=150,bbox_inches='tight')
